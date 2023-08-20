5bb73681a2a18be27eb19e55323de8a2dffe4421
Graham Williams <Graham.Williams@togaware.com>
2023-08-21 06:09:52 +1000

    Constants into own file, std location.

--- lib/constants/app.dart
+++ lib/constants/app.dart
@@ -1,12 +1,35 @@
-const cmdLogIntro = """\
-The log captures all interactions with Rattle as an R script.
+/// App-Wide Constants for RattleNG
+///
+/// Copyright (C) 2023, Togaware Pty Ltd.
+///
+/// License: https://www.gnu.org/licenses/gpl-3.0.en.html
+///
+// Licensed under the GNU General Public License, Version 3 (the "License");
+///
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <https://www.gnu.org/licenses/>.
+///
+/// Authors: Graham Williams
 
-You can Save the log to a file (e.g., 'model.R') to run in R, independent of
-Rattle.To run the file as an R script use the Rscript command. Alternatively,
-through the R Console or RStudio run the command using "source('model.R')".
-""";
+/// Project assets folder path used in the app.
 
-var cmdInitialMessage = """\
+const String assetsPath = 'assets';
+
+const String rattleWelcome = '$assetsPath/markdown/rattle_welcome.md';
+
+const String logIntro = '$assetsPath/markdown/log_intro.md';
+
+final String cmdInitialMessage = """\
 #=======================================================================
 #
 # Rattle is Copyright (c) 2006-${DateTime.now().year} Togaware Pty Ltd.
@@ -47,4 +70,3 @@ scoring  <- ! building
 
 set.seed(42)
 """;
-
