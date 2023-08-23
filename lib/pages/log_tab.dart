7c5b83775369a5989ba495ff93c2689c330d1298
Graham Williams <Graham.Williams@togaware.com>
2023-08-23 20:57:22 +1000

    Exploring how to add text to Text.

--- lib/pages/log_tab.dart
+++ lib/pages/log_tab.dart
@@ -5,7 +5,7 @@
 /// License: GNU General Public License, Version 3 (the "License")
 /// https://www.gnu.org/licenses/gpl-3.0.en.html
 //
-// Time-stamp: <Wednesday 2023-08-23 12:02:52 +1000 Graham Williams>
+// Time-stamp: <Wednesday 2023-08-23 15:22:08 +1000 Graham Williams>
 //
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
@@ -30,8 +30,11 @@ import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:rattle/constants/app.dart';
+import 'package:rattle/helpers/r.dart';
 import 'package:rattle/widgets/save_log_button.dart';
 import 'package:rattle/widgets/markdown_file.dart';
+
+final logController = TextEditingController();
 
 class LogTab extends StatelessWidget {
   const LogTab({Key? key}) : super(key: key);
@@ -60,10 +63,17 @@ class LogTab extends StatelessWidget {
             flex: 7,
             child: SingleChildScrollView(
               scrollDirection: Axis.vertical,
+
+              // A SelectableText to allow for the widget contents to be added
+              // to.
+
               child: SelectableText(
+                //child: TextField(
                 // TODO AVOID FILENAME LITERALS IN THE CODE
                 File("assets/scripts/main.R").readAsStringSync(),
+                // WANT THIS BUT HOW????   controller: logController,
                 key: const Key('log_text'),
+                //readOnly: true,
                 style: const TextStyle(
                   // fontFamily: 'UbuntuMono',
                   // fontSize: 14,
