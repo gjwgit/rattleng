6ceb6feefbc7a9054e2d4eac5ee986cccf06202c
Graham Williams <Graham.Williams@togaware.com>
2023-08-23 20:55:59 +1000

    Add R statup to main.

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@
 /// License: GNU General Public License, Version 3 (the "License")
 /// https://www.gnu.org/licenses/gpl-3.0.en.html
 //
-// Time-stamp: <Wednesday 2023-08-23 12:00:12 +1000 Graham Williams>
+// Time-stamp: <Wednesday 2023-08-23 14:44:37 +1000 Graham Williams>
 //
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
@@ -30,6 +30,7 @@ import 'package:flutter/material.dart';
 import 'package:universal_io/io.dart' show Platform;
 import 'package:window_manager/window_manager.dart';
 
+import 'package:rattle/helpers/r.dart';
 import 'package:rattle/pages/home_page.dart';
 import 'package:rattle/widgets/material_color.dart';
 
@@ -62,6 +63,7 @@ void main() async {
   }
 
   print("STARTUP THE R PROCESS HERE AND `source('assets/scripts/main.R')`");
+  rStart();
 
   runApp(const RattleApp());
 }
