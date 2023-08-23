ac5838adcf83849a9e9381d25247645ac54b044f
Graham Williams <Graham.Williams@togaware.com>
2023-08-23 12:04:35 +1000

    Replace fixed intro R script with main.R

--- lib/constants/app.dart
+++ lib/constants/app.dart
@@ -5,7 +5,7 @@
 /// License: https://www.gnu.org/licenses/gpl-3.0.en.html
 ///
 //
-// Time-stamp: <Monday 2023-08-21 08:40:56 +1000 Graham Williams>
+// Time-stamp: <Wednesday 2023-08-23 09:41:55 +1000 Graham Williams>
 //
 // Licensed under the GNU General Public License, Version 3 (the "License");
 ///
@@ -31,45 +31,3 @@ const String assetsPath = 'assets';
 const String rattleWelcome = '$assetsPath/markdown/rattle_welcome.md';
 
 const String logIntro = '$assetsPath/markdown/log_intro.md';
-
-final String cmdInitialMessage = """\
-#=======================================================================
-#
-# Rattle is Copyright (c) 2006-${DateTime.now().year} Togaware Pty Ltd.
-# It is free (as in libre) open source software.
-# It is licensed under the GNU General Public License,
-# Version 3. Rattle comes with ABSOLUTELY NO WARRANTY.
-# Rattle was written by Graham Williams with contributions
-# from others as acknowledged in 'library(help=rattle)'.
-# Visit https://rattle.togaware.com/ for details.
-#
-#=======================================================================
-#
-# Rattle timestamp: ${DateTime.now()} x86_64-pc-linux-gnu 
-#
-# Rattle version 6.0.0 user 'gjw'
-#
-# We begin most scripts by loading the required packages.
-# Here are some initial packages to load and others will be
-# identified as we proceed through the script. When writing
-# our own scripts we often collect together the library
-# commands at the beginning of the script here.
-
-library(rattle)   # Access the weather dataset and utilities.
-library(magrittr) # Utilise %>% and %<>% pipeline operators.
-  
-# This log generally records the process of building a model. 
-# However, with very little effort the log can also be used 
-# to score a new dataset. The logical variable 'building' 
-# is used to toggle between generating transformations, 
-# when building a model and using the transformations, 
-# when scoring a dataset.
-
-building <- TRUE
-scoring  <- ! building
-
-# A pre-defined value is used to reset the random seed 
-# so that results are repeatable.
-
-set.seed(42)
-""";
