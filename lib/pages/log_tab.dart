db729299e7d0e1e9e05a1a82446f89c7742e3776
Graham Williams <Graham.Williams@togaware.com>
2023-08-27 12:47:27 +1000

    Update to load_asset.

--- lib/pages/log_tab.dart
+++ lib/pages/log_tab.dart
@@ -5,7 +5,7 @@
 /// License: GNU General Public License, Version 3 (the "License")
 /// https://www.gnu.org/licenses/gpl-3.0.en.html
 //
-// Time-stamp: <Wednesday 2023-08-23 15:22:08 +1000 Graham Williams>
+// Time-stamp: <Sunday 2023-08-27 12:46:29 +1000 Graham Williams>
 //
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
