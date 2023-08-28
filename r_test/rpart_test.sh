9a7ecdb375b98aab74b667309b1de72e2caddc19
Graham Williams <Graham.Williams@togaware.com>
2023-08-29 05:45:17 +1000

    Add PARAMS for rpart options to the mapping.

--- r_test/rpart_test.sh
+++ r_test/rpart_test.sh
@@ -9,6 +9,11 @@ s|<<VAR_TARGET>>|rain_tomorrow|;
 s|<<VAR_RISK>>|risk_mm|;
 s|<<VARS_ID>>|"date", "location"|;
 s|<<DATA_SPLIT_TR_TU_TE>>|0.70, 0.15, 0.15|g;
+s|<<PRIORS>>||;
+s|<<LOSS>>||;
+s|<<MINSPLIT>>||;
+s|<<MINBUCKET>>||;
+s|<<CP>>||;
 print unless /^#|^$|^<</
 ' \
      ${RSCRIPTS}/main.R \
