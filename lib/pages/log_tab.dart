16da90e59f8af1dcfba401774f48aa9d88878c0b
Graham Williams <Graham.Williams@togaware.com>
2023-09-01 07:46:20 +1000

    Review

--- lib/pages/log_tab.dart
+++ lib/pages/log_tab.dart
@@ -5,7 +5,7 @@
 /// License: GNU General Public License, Version 3 (the "License")
 /// https://www.gnu.org/licenses/gpl-3.0.en.html
 //
-// Time-stamp: <Monday 2023-08-28 08:55:26 +1000 Graham Williams>
+// Time-stamp: <Tuesday 2023-08-29 08:18:30 +1000 Graham Williams>
 //
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
