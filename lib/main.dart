29f289f2cb32852954ccca0cd8eee6e38071bbb2
Graham Williams <Graham.Williams@togaware.com>
2023-09-09 17:56:38 +1000

    Rename createMaterialColor to helper.

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@
 /// License: GNU General Public License, Version 3 (the "License")
 /// https://www.gnu.org/licenses/gpl-3.0.en.html
 //
-// Time-stamp: <Saturday 2023-09-09 16:27:35 +1000 Graham Williams>
+// Time-stamp: <Saturday 2023-09-09 17:56:25 +1000 Graham Williams>
 //
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
@@ -26,10 +26,10 @@ import 'package:flutter/material.dart';
 
 import 'package:window_manager/window_manager.dart';
 
+import 'package:rattle/helpers/create_material_color.dart';
 import 'package:rattle/helpers/r.dart' show rStart;
 import 'package:rattle/helpers/utils.dart';
 import 'package:rattle/pages/home_page.dart';
-import 'package:rattle/widgets/material_color.dart';
 
 void main() async {
   // The `main` entry point into any dart app.
