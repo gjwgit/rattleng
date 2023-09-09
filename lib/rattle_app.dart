9b8000436d5708829b48a0c647a59c0c78b4913f
Graham Williams <Graham.Williams@togaware.com>
2023-09-09 20:27:20 +1000

    Split the root app to it's own file.

---
+++ lib/rattle_app.dart
@@ -0,0 +1,47 @@
+/// The root widget for the Rattle app.
+///
+/// Copyright (C) 2023, Togaware Pty Ltd.
+///
+/// License: GNU General Public License, Version 3 (the "License")
+/// https://www.gnu.org/licenses/gpl-3.0.en.html
+//
+// Time-stamp: <Saturday 2023-09-09 20:26:51 +1000 Graham Williams>
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <https://www.gnu.org/licenses/>.
+///
+/// Authors: Graham Williams
+
+import 'package:flutter/material.dart';
+
+import 'package:rattle/helpers/create_material_color.dart';
+import 'package:rattle/pages/home_page.dart';
+
+class RattleApp extends StatelessWidget {
+  const RattleApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: createMaterialColor(const Color(0xff45035e)),
+        textTheme: Theme.of(context).textTheme.apply(
+              fontSizeFactor: 1.1,
+              fontSizeDelta: 2.0,
+            ),
+      ),
+      home: const RattleHomePage(),
+    );
+  }
+}
