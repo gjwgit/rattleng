4cd67a839d91482ed972466a987709c54af99e40
Graham Williams <Graham.Williams@togaware.com>
2023-09-09 16:21:15 +1000

    Initial console tab implementation

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@
 /// License: GNU General Public License, Version 3 (the "License")
 /// https://www.gnu.org/licenses/gpl-3.0.en.html
 //
-// Time-stamp: <Saturday 2023-09-09 10:00:36 +1000 Graham Williams>
+// Time-stamp: <Saturday 2023-09-09 14:20:43 +1000 Graham Williams>
 //
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
@@ -24,42 +24,54 @@
 
 import 'dart:io';
 
-import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 
-import 'package:universal_io/io.dart' show Platform;
 import 'package:window_manager/window_manager.dart';
 
 import 'package:rattle/helpers/r.dart' show rStart;
+import 'package:rattle/helpers/utils.dart';
 import 'package:rattle/pages/home_page.dart';
 import 'package:rattle/widgets/material_color.dart';
 
 void main() async {
+  // The `main` entry point into any dart app.
+  //
+  // We use async because WHY TODO.
+
   // Use debugPrint() to print trace messages and backtraces in preference to
   // print(). Use print() for human readable messages to the console for errors,
   // like login credentials error. The debugPrint() output is then able to be
-  // globally suppressed with the following noop redefinition. 20220512 gjw
+  // globally suppressed with the following noop redefinition. We also use an
+  // environment through helper/utils.dart to toggle this externally, often
+  // through a Makefile. 20220512 gjw
 
-  // ignore: no-empty-block
-  // debugPrint = (String? message, {int? wrapWidth}) {};
-
-  // Identify if Desktop or Mobile app.
-
-  bool isDesktop = Platform.isLinux || Platform.isMacOS || Platform.isWindows;
+  if (DebugPrintConfig.debugPrint == 'FALSE') {
+    // ignore: no-empty-block
+    debugPrint = (String? message, {int? wrapWidth}) {};
+  }
 
   // Tune the window manager before runApp to avoid a lag in the UI. For desktop
-  // (non-web) versions re-size to mimic mobile (as the main target platform).
+  // (non-web) versions re-size to a comfortable initial window.
 
-  if (isDesktop && !kIsWeb) {
+  if (isDesktop) {
     WidgetsFlutterBinding.ensureInitialized();
 
     await windowManager.ensureInitialized();
 
     WindowOptions windowOptions = const WindowOptions(
+      // Setting [alwaysOnTop] here will ensure the app starts on top of other
+      // apps on the desktop so that it is visible. We later turn it of as we
+      // don;t want to force it always on top.
+
       alwaysOnTop: true,
+
       // The size is overriden in the first instance by linux/my_application.cc
-      // but then does has effect when Retarting the app.
+      // but setting it here then does have effect when Retarting the app.
+
       size: Size(950, 600),
+
+      // The [title] is used for the window manager's window title.
+
       title: "RattleNG - Data Science with R",
     );
 
