17ba047ccd08c7071d06ad7921386e14215a7fb7
Graham Williams <Graham.Williams@togaware.com>
2023-09-12 08:37:39 +1000

    Data tab consumes the dataset model to toggle main window.

--- lib/tabs/data_tab.dart
+++ lib/tabs/data_tab.dart
@@ -5,7 +5,7 @@
 /// License: https://www.gnu.org/licenses/gpl-3.0.en.html
 ///
 //
-// Time-stamp: <Monday 2023-09-11 08:12:13 +1000 Graham Williams>
+// Time-stamp: <Tuesday 2023-09-12 08:31:46 +1000 Graham Williams>
 //
 // Licensed under the GNU General Public License, Version 3 (the "License");
 //
@@ -26,7 +26,10 @@
 
 import 'package:flutter/material.dart';
 
+import 'package:provider/provider.dart';
+
 import 'package:rattle/constants/app.dart';
+import 'package:rattle/models/dataset_model.dart';
 import 'package:rattle/widgets/dataset_chooser.dart';
 import 'package:rattle/widgets/markdown_file.dart';
 
@@ -40,23 +43,42 @@ class DataTab extends StatefulWidget {
 class DataTabState extends State<DataTab> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          const DatasetChooser(),
+    return Consumer<DatasetModel>(
+      // Build a [Consumer] of the [DatasetModel] so we can access updated
+      // values of the path variable.
 
-          // A text view that takes up the remaining space and displays the
-          // Rattle welcome and getting started message. This will be
-          // overwritten once a dataset is loaded.
+      builder: (context, dataset, child) {
+        return Scaffold(
+          body: Column(
+            children: [
+              const DatasetChooser(),
 
-          Expanded(
-            child: Center(
-              key: const Key("rattle_welcome"),
-              child: sunkenMarkdownFileBuilder(welcomeMsgFile),
-            ),
+              // A text view that takes up the remaining space and displays the
+              // Rattle welcome and getting started message. This will be
+              // overwritten once a dataset is loaded.
+
+              Visibility(
+                visible: dataset.path == "",
+                child: Expanded(
+                  child: Center(
+                    key: const Key("rattle_welcome"),
+                    child: sunkenMarkdownFileBuilder(welcomeMsgFile),
+                  ),
+                ),
+              ),
+              Visibility(
+                visible: dataset.path != "",
+                child: Expanded(
+                  child: Center(
+                    child: Text(
+                        "DISPLAY OUTPUT OF glimpse(ds) HERE AS NEXT APPROXIMATION 20230912"),
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
