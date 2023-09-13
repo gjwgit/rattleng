f690f81efff2c3dfe2e952ca50c024303825a62c
Graham Williams <Graham.Williams@togaware.com>
2023-09-13 19:36:18 +1000

    Do not output scripts to debugPrint.

--- lib/helpers/r_start.dart
+++ lib/helpers/r_start.dart
@@ -5,7 +5,7 @@
 /// License: GNU General Public License, Version 3 (the "License")
 /// https://www.gnu.org/licenses/gpl-3.0.en.html
 //
-// Time-stamp: <Wednesday 2023-09-13 17:09:17 +1000 Graham Williams>
+// Time-stamp: <Wednesday 2023-09-13 17:27:06 +1000 Graham Williams>
 //
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
@@ -49,8 +49,8 @@ void rStart() async {
   // Comment out the following to then only print debugPrint messages to the
   // console.
 
-  process.stdout.transform(utf8.decoder).forEach(debugPrint);
-  process.stderr.transform(utf8.decoder).forEach(debugPrint);
+  //process.stdout.transform(utf8.decoder).forEach(debugPrint);
+  //process.stderr.transform(utf8.decoder).forEach(debugPrint);
 
   // Read the main R startup code from the script file.
 
