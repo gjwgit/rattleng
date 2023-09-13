18544e5632984892bc2ff3648a2f6bf1c3aaff1d
Graham Williams <Graham.Williams@togaware.com>
2023-09-13 17:02:40 +1000

    Pass rattle model on for updating script.

--- lib/helpers/r_load_dataset.dart
+++ lib/helpers/r_load_dataset.dart
@@ -1,6 +1,31 @@
+/// Load a dataset into R.
+///
+/// Copyright (C) 2023, Togaware Pty Ltd.
+///
+/// License: GNU General Public License, Version 3 (the "License")
+/// https://www.gnu.org/licenses/gpl-3.0.en.html
+//
+// Time-stamp: <Wednesday 2023-09-13 16:02:04 +1000 Graham Williams>
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <https://www.gnu.org/licenses/>.
+///
+/// Authors: Graham Williams
+
 import 'package:flutter/material.dart';
 
-import 'package:rattle/helpers/r.dart' show rSource;
+import 'package:rattle/helpers/r_source.dart';
+import 'package:rattle/models/rattle_model.dart';
 
 /// Load the specified dataset using the appropriate R script.
 ///
@@ -10,8 +35,9 @@ import 'package:rattle/helpers/r.dart' show rSource;
 /// different in the case where the dataset variables have been normalised,
 /// which is the default.
 
-// SHOULD BE void loadDataset(String filename) {
-void loadDataset(String filename) {
+void rLoadDataset(String filename, RattleModel rattle) {
+  String code = "";
+
   // Get the filename from the corresponding widget.
 
   // final dsPathTextFinder = find.byKey(const Key('ds_path_text'));
@@ -27,21 +53,20 @@ void loadDataset(String filename) {
 
   if (filename == '' || filename == 'rattle::weather') {
     debugPrint('LOAD_DATASET: rattle::weather');
-    rSource("data_load_weather");
+    rSource("data_load_weather", rattle);
   } else {
     debugPrint('LOAD_DATASET: FILENAME IS NOT RECOGNISED SO ABORT');
-    return;
   }
   debugPrint('LOAD_DATASET: FILENAME IS NOT EMPTY SO FOR NOW:');
   rSource(
-    "data_template",
+    "data_template", rattle,
     // {
     //   "VAR_TARGET": "rain_tomorrow",
     //   "VAR_RISK": "risk_mm",
     //   "VARS_ID": '"date", "location"'
     // }
   );
-  rSource('ds_summarise');
+  rSource('ds_summarise', rattle);
   debugPrint(
     'LOAD_DATASET: OTHERWISE WE SHOULD RUN APPROPRIATE R SCRIPT '
     'TO LOAD DATA FROM -> "$filename";',
