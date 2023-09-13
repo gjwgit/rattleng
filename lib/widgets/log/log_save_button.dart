ffbedeabe95ad1bac6c5410608cc7a2a0837b35a
Graham Williams <Graham.Williams@togaware.com>
2023-09-13 19:37:06 +1000

    Export rattle.script.

--- lib/widgets/log/log_save_button.dart
+++ lib/widgets/log/log_save_button.dart
@@ -5,7 +5,7 @@
 /// License: GNU General Public License, Version 3 (the "License")
 /// https://www.gnu.org/licenses/gpl-3.0.en.html
 ///
-// Time-stamp: <Wednesday 2023-09-13 09:07:38 +1000 Graham Williams>
+// Time-stamp: <Wednesday 2023-09-13 19:33:26 +1000 Graham Williams>
 //
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
@@ -26,45 +26,23 @@ import 'dart:io' show File;
 
 import 'package:flutter/material.dart';
 
-// TODO MOVE TO provider USING script INSIDE RATTLE DATA MODEL.
+import 'package:provider/provider.dart';
 
-// ignore:  depend_on_referenced_packages
-import 'package:flutter_test/flutter_test.dart' show find;
+import 'package:rattle/models/rattle_model.dart';
 
 class LogSaveButton extends StatelessWidget {
   const LogSaveButton({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      child: const Text("Export"),
-      onPressed: () {
-        debugPrint("SAVE SCRIPT TO SCRIPT.R");
-        debugPrint("TODO PROMPT FOR THE NAME TO SAVE TO.");
-
-        //var tv = find.byType(Text);
-        //var tv = find.textContaining(RegExp(r"^#===.*"));
-        var tv = find.byKey(const Key('log_text'));
-        //debugPrint(tv.evaluate());
-        // How to extract the text????
-        //File('script.R').writeAsString(tv.evaluate().toString());
-
-        //var ts = tv.data;
-        var ts = tv.evaluate().toString();
-        // ts = ts.replaceFirst(RegExp(r"^[^#]*#"), '    #');
-        // ts = ts.replaceFirst(RegExp(r"  , style:.*"), '');
-        // ts = ts.replaceAll(RegExp(r"\\n   "), '');
-
-        File('script.R').writeAsString(ts);
-        debugPrint("LOG WRITTEN TO `script.R`");
-
-        //var tv = find.byKey(Key('log_text')).evaluate().first.widget; => "LogTab"
-        //var tv = find.byKey(Key('log_text')).evaluate().first;
-        //var tv = find.byKey(Key('log_text')).evaluate();
-        //File('script.R').writeAsString(tv.toString());
-        //var tx = tv.evaluate().first.widget;
-        //debugPrint(tx);
-        // File('script.R').writeAsString(tx);
+    return Consumer<RattleModel>(
+      builder: (context, rattle, child) {
+        return ElevatedButton(
+          child: const Text("Export"),
+          onPressed: () {
+            File('script.R').writeAsString(rattle.script);
+          },
+        );
       },
     );
   }
