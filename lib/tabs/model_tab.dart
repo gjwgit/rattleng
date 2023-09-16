dcfa8cd4a25c89adf95818947f84de8d0c576489
Graham Williams <Graham.Williams@togaware.com>
2023-09-17 06:11:39 +1000

    Add model radio buttons.

--- lib/tabs/model_tab.dart
+++ lib/tabs/model_tab.dart
@@ -1,11 +1,11 @@
-/// First and Dataset page for the first Data tab.
+/// Model tab for home page.
 ///
 /// Copyright (C) 2023, Togaware Pty Ltd.
 ///
 /// License: https://www.gnu.org/licenses/gpl-3.0.en.html
 ///
 //
-// Time-stamp: <Friday 2023-09-15 21:19:21 +1000 Graham Williams>
+// Time-stamp: <Sunday 2023-09-17 05:58:52 +1000 Graham Williams>
 //
 // Licensed under the GNU General Public License, Version 3 (the "License");
 //
@@ -28,13 +28,13 @@ import 'package:flutter/material.dart';
 
 import 'package:provider/provider.dart';
 
-import 'package:rattle/constants/app.dart';
-import 'package:rattle/helpers/r_extract_glimpse.dart';
-import 'package:rattle/helpers/r_extract_print_rpart.dart';
+import 'package:rattle/helpers/r_extract.dart';
 import 'package:rattle/helpers/r_source.dart';
 import 'package:rattle/models/rattle_model.dart';
-import 'package:rattle/widgets/dataset_chooser.dart';
-import 'package:rattle/widgets/markdown_file.dart';
+import 'package:rattle/widgets/model_radio_buttons.dart';
+import 'package:rattle/widgets/model_buttons.dart';
+
+// TODO 20230916 gjw DOES THIS NEED TO BE STATEFUL?
 
 class ModelTab extends StatefulWidget {
   const ModelTab({Key? key}) : super(key: key);
@@ -47,35 +47,33 @@ class ModelTabState extends State<ModelTab> {
   @override
   Widget build(BuildContext context) {
     return Consumer<RattleModel>(
-      // Build a [Consumer] of the [RattleModel] so we can access updated
-      // values of the path variable.
-
+      // A [Consumer] of the [RattleModel] so we can access updated values of
+      // the stdout variable.
       builder: (context, rattle, child) {
         return Scaffold(
-          body: Column(children: [
-            ElevatedButton(
-              onPressed: () async {
-                rSource("model_template", rattle);
-                rSource("rpart_build", rattle);
-              },
-              child: const Tooltip(
-                message: "Click here to build rpart for now.",
-                child: Text("Build Rpart"),
-              ),
-            ),
-            Expanded(
-              child: Container(
-                padding: const EdgeInsets.only(left: 0),
-                child: SelectableText(
-                  rExtractPrintRpart(rattle.stdout),
-                  style: const TextStyle(
-                    fontFamily: 'RobotoMono',
-                    fontSize: 16,
+          body: Column(
+            children: [
+              ModelRadioButtons(),
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.only(left: 0),
+                  child: SingleChildScrollView(
+                    child: SelectableText(
+                      // ignore: prefer_interpolation_to_compose_strings
+                      rExtract(rattle.stdout, "> print(model_rpart)") +
+                          "\n" +
+                          rExtract(rattle.stdout, "> printcp(model_rpart)") +
+                          "\n",
+                      style: const TextStyle(
+                        fontFamily: 'RobotoMono',
+                        fontSize: 16,
+                      ),
+                    ),
                   ),
                 ),
               ),
-            ),
-          ]),
+            ],
+          ),
         );
       },
     );
---
+++ lib/widgets/model_radio_buttons.dart
@@ -0,0 +1,94 @@
+/// Radio buttons to choose the model ti build.
+///
+/// Copyright (C) 2023, Togaware Pty Ltd.
+///
+/// License: https://www.gnu.org/licenses/gpl-3.0.en.html
+///
+// Licensed under the GNU General Public License, Version 3 (the "License");
+///
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <https://www.gnu.org/licenses/>.
+///
+/// Authors: Graham Williams
+
+import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
+
+import 'package:rattle/helpers/r_source.dart';
+import 'package:rattle/models/rattle_model.dart';
+
+class ModelRadioButtons extends StatefulWidget {
+  @override
+  _ModelRadioButtonsState createState() => _ModelRadioButtonsState();
+}
+
+class _ModelRadioButtonsState extends State<ModelRadioButtons> {
+  // List of modellers we support.
+
+  List<String> modellers = ['Cluster', 'Associate', 'Tree', 'Forest', 'Boost'];
+
+  // Default selected valueas an idex into the modellers.
+
+  int selectedValue = 2;
+
+  void selectRadio(int value) {
+    setState(() {
+      selectedValue = value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Row(
+          children: modellers.asMap().entries.map((entry) {
+            int index = entry.key;
+            String label = entry.value;
+            return buildRadioTile(index, label);
+          }).toList(),
+        ),
+        SizedBox(width: 20), // Add some spacing
+        ElevatedButton(
+          onPressed: () {
+            // Handle button click here
+            debugPrint("BUTTON CLICKED! SELECTED VALUE "
+                "$selectedValue = ${modellers[selectedValue]}");
+          },
+          child: Text('Build'),
+        ),
+      ],
+    );
+  }
+
+  Widget buildRadioTile(int value, String label) {
+    return GestureDetector(
+      onTap: () {
+        selectRadio(value);
+      },
+      child: Row(
+        children: [
+          Radio(
+            value: value,
+            groupValue: selectedValue,
+            onChanged: (int? newValue) {
+              selectRadio(newValue!);
+            },
+          ),
+          Text(label),
+        ],
+      ),
+    );
+  }
+}
