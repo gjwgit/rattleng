afe53cb946b5e87c455eefaba0d06d3dd61a4934
Graham Williams <Graham.Williams@togaware.com>
2023-10-15 16:52:41 +1100

    Update integration testing to a single file.

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -15,6 +15,9 @@
 ///
 /// Authors: Graham Williams
 
+// TODO 20231015 gjw MIGRATE ALL TESTS TO THE ONE APP INSTANCE RATHER THAN A
+// COSTLY BUILD EACH INDIVIDUAL TEST!
+
 import 'dart:io';
 
 import 'package:flutter/material.dart';
@@ -25,8 +28,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 import 'package:rattle/constants/keys.dart';
-import 'package:rattle/main.dart' as rattle;
+import 'package:rattle/main.dart' as app;
 import 'package:rattle/dataset/button.dart';
+import 'package:rattle/dataset/popup.dart';
 
 /// A duration to allow the tester to view/interact with the testing. 5s is
 /// good, 10s is useful for development and 0s for ongoing. This is not
@@ -48,7 +52,7 @@ void main() {
   testWidgets('Home page loads okay.', (WidgetTester tester) async {
     debugPrint("TESTER: Start up the app");
 
-    rattle.main();
+    app.main();
 
     // Trigger a frame. Finish animation and scheduled microtasks.
 
@@ -88,6 +92,103 @@ void main() {
 
     final statusBarFinder = find.byKey(statusBarKey);
     expect(statusBarFinder, findsOneWidget);
+
+    ////////////////////////////////////////////////////////////////////////
+    // DATASET DEMO
+    ////////////////////////////////////////////////////////////////////////
+
+    debugPrint("TESTER: Tap the Dataset button.");
+
+    final datasetButton = find.byType(DatasetButton);
+    expect(datasetButton, findsOneWidget);
+    await tester.pump(pause);
+    await tester.tap(datasetButton);
+    await tester.pumpAndSettle();
+    // Always delay here since if not the glimpse view is not available in
+    // time! Odd but that's the result of experimenting. Have a delay after
+    // the Demo buttons is pushed does not get the glimpse contents into the
+    // widget.
+    await tester.pump(delay);
+
+    debugPrint("TESTER: Tap the Demo button.");
+
+    final datasetPopup = find.byType(DatasetPopup);
+    expect(datasetPopup, findsOneWidget);
+    final demoButton = find.text("Demo");
+    expect(demoButton, findsOneWidget);
+    await tester.tap(demoButton);
+    await tester.pumpAndSettle();
+    await tester.pump(pause);
+
+    debugPrint("TESTER: Expect the default demo dataset is identified.");
+
+    final dsPathTextFinder = find.byKey(datasetPathKey);
+    expect(dsPathTextFinder, findsOneWidget);
+    final dsPathText = dsPathTextFinder.evaluate().first.widget as TextField;
+    String filename = dsPathText.controller?.text ?? '';
+    expect(filename, "rattle::weather");
+
+    debugPrint("TESTER: Check welcome hidden and dataset is visible.");
+
+    final datasetFinder = find.byType(Visibility);
+    expect(datasetFinder, findsNWidgets(2));
+    expect(
+      datasetFinder.evaluate().first.widget.toString(),
+      contains("hidden"),
+    );
+    expect(
+      datasetFinder.evaluate().last.widget.toString(),
+      contains("visible"),
+    );
+
+    debugPrint("TESTER: Expect the default demo dataset is loaded.");
+
+    final datasetDisplayFinder = find.byKey(datasetGlimpseKey);
+
+    expect(datasetDisplayFinder, findsOneWidget);
+
+    final dataset =
+        datasetDisplayFinder.evaluate().last.widget as SelectableText;
+
+    expect(dataset.data, contains("Rows: 366\n"));
+    expect(dataset.data, contains("Columns: 24\n"));
+    expect(dataset.data, contains("date            <date>"));
+    expect(dataset.data, contains("rain_tomorrow   <fct>"));
+
+    debugPrint("TESTER TODO: Confirm the status bar has been updated.");
+
+    debugPrint("TESTER: Check R script widget contains the expected code.");
+
+    final scriptTabFinder = find.text('Script');
+    expect(scriptTabFinder, findsOneWidget);
+    await tester.tap(scriptTabFinder);
+    await tester.pumpAndSettle();
+    await tester.pump(pause);
+
+    final scriptTextFinder = find.byKey(scriptTextKey);
+    expect(
+      scriptTextFinder.first.toString(),
+      contains('## -- main.R --'),
+    );
+    expect(
+      scriptTextFinder.first.toString(),
+      contains('## -- data_load_weather.R --'),
+    );
+    expect(
+      scriptTextFinder.first.toString(),
+      contains('## -- data_template.R --'),
+    );
+    expect(
+      scriptTextFinder.first.toString(),
+      contains('## -- ds_glimpse.R --'),
+    );
+
+    debugPrint("TESTER TODO: Tap Export. Check/run export.R.");
+
+    debugPrint("TESTER TODO: From Dataset tab uncheck Normalise and reload.");
+
+    // This will test if Date is the first column rather than date. Also
+    // RainTomorrow rather than rain_tomorrow.
 
     debugPrint("TESTER: Finished.");
   });
