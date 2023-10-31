4f1aeb74dae853c0fbd2cec10770c5e37e7fd487
Graham Williams <graham.williams@togaware.com>
2023-11-01 08:48:59 +1100

    Add provider for the status message.

---
+++ lib/provider/status.dart
@@ -0,0 +1,29 @@
+/// Gloabl variable status as text for the status bar.
+///
+/// Copyright (C) 2023, Togaware Pty Ltd.
+///
+/// License: GNU General Public License, Version 3 (the "License")
+/// https://www.gnu.org/licenses/gpl-3.0.en.html
+//
+// Time-stamp: <Wednesday 2023-11-01 05:49:23 +1100 Graham Williams>
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <https://www.gnu.org/licenses/>.
+///
+/// Authors: Graham Williams
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:rattle/constants/status.dart';
+
+final statusProvider = StateProvider<String>((ref) => statusWelcomeMsg);
