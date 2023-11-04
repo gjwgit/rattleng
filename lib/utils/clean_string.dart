c87061c926109977da34f9d4ec4832600285f760
Graham Williams <graham.williams@togaware.com>
2023-11-05 05:24:47 +1100

    Clean the strings generated from pty now.

---
+++ lib/utils/clean_string.dart
@@ -0,0 +1,19 @@
+String cleanString(String txt) {
+  // On moving to pty I was getting lots of escapes.
+
+  txt = txt.replaceAll('[3m[38;5;246m', '');
+  txt = txt.replaceAll('[?2004l', '');
+  txt = txt.replaceAll('[39m[23m', '');
+  txt = txt.replaceAll('[?2004h', '');
+  txt = txt.replaceAll('[A', '');
+  txt = txt.replaceAll('[C', '');
+  txt = txt.replaceAll('[?25h', '');
+  txt = txt.replaceAll('[K', '');
+  txt = txt.replaceAll('[32m', '');
+  txt = txt.replaceAll('[34m', '');
+  txt = txt.replaceAll('[39m', '');
+  txt = txt.replaceAll('[31m', '');
+  txt = txt.replaceAll('', '');
+
+  return txt;
+}
