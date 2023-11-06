0518a68559a495ca3d750f104f26c51b91320b28
Graham Williams <graham.williams@togaware.com>
2023-11-06 13:33:54 +1100

    Calculate rpart method in R script.

--- assets/r/model_build_rpart.R
+++ assets/r/model_build_rpart.R
@@ -5,7 +5,7 @@
 # License: GNU General Public License, Version 3 (the "License")
 # https://www.gnu.org/licenses/gpl-3.0.en.html
 #
-# Time-stamp: <Sunday 2023-10-08 15:07:06 +1100 Graham Williams>
+# Time-stamp: <Monday 2023-11-06 13:31:31 +1100 Graham Williams>
 #
 # Licensed under the GNU General Public License, Version 3 (the "License");
 #
@@ -44,10 +44,13 @@ library(rpart)        # ML: decision tree rpart().
 mtype <- "rpart"
 mdesc <- "Tree"
 
+method <- ifelse(ds[[target]] %>% unique() %>% length() > 10,
+                 "anova", "class")
+
 model_rpart <- rpart(
   form,
   data=ds[tr, vars],
-  method="class",
+  method=method,
   parms=list(split="information" PRIORS LOSS),
   control=rpart.control(usesurrogate=0,
                         maxsurrogate=0 MINSPLIT MINBUCKET CP),
