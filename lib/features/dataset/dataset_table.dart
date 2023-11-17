3570fd70eee64bf892e29437488708b1169375b7
Mukund Srinivas <u7274095@anu.edu.au>
2023-11-17 19:27:15 +1100

    Implemented preliminary dataset tab layout with three columns, incorporating a dropdown in the third column.
    Next set of issues to work on:
    1. The change in dropdown state has to be handled by a stateful widget.
    2. Working with `glimpse(ds)` to get the relevant information regarding data.

--- lib/features/dataset/dataset_table.dart
+++ lib/features/dataset/dataset_table.dart
@@ -5,21 +5,56 @@ class DataTableWidget extends StatelessWidget {
 
   DataTableWidget(this.columnNames, {Key? key}) : super(key: key);
 
-  // A method to get the data from the R Script and parse it to make rows
-  
-  List<DataRow> makeRows(){
-       return DataRow(cells: []).toList();
+  // A method to make rows from variable names
+  // In a given row , the first element is the row name ,
+  //The second element is the datatype
+  //The third element is a drop down with 5 options {Input , Text , Risk , Ident , Weight}
+  List<DataRow> makeVarNames(List<String> varNames) {
+    return varNames.map((String varName) {
+      return DataRow(
+        cells: [
+          DataCell(Text(varName)),
+          DataCell(const Text("Type")),
+          DataCell(
+            DropdownButton<String>(
+              value: "input",
+              items: [
+                DropdownMenuItem(
+                  child: Text("Input"),
+                  value: 'input',
+                ),
+                DropdownMenuItem(
+                  child: Text("Target"),
+                  value: 'text',
+                ),
+                DropdownMenuItem(
+                  child: Text("Risk"),
+                  value: 'risk',
+                ),
+                DropdownMenuItem(
+                  child: Text("Ident"),
+                  value: 'weight',
+                ),
+              ],
+              onChanged: (String? value) {
+                print(value);
+              },
+            ),
+          ),
+        ],
+      );
+    }).toList();
   }
 
   @override
   Widget build(BuildContext context) {
     return DataTable(
-      columns:[
-        DataColumn(label: Text('Variable'))
-        DataColumn(label: Text('DataType'))
+      columns: [
+        DataColumn(label: Text('Variable')),
+        DataColumn(label: Text('DataType')),
+        DataColumn(label: Text('Role'))
       ],
-      rows: [],
-      
+      rows: makeVarNames(["a", "b", "c"].toList()),
     );
   }
 }
