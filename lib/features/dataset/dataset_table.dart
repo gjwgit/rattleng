baddb629896a98b7452c8699f2ca5c2ef9de276d
Mukund Srinivas <u7274095@anu.edu.au>
2023-11-19 09:51:37 +1100

    Stateful widget implemented
    1.The role of the variable changes the state of the widget

--- lib/features/dataset/dataset_table.dart
+++ lib/features/dataset/dataset_table.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
 
-class DataTableWidget extends StatelessWidget {
+class DataTableWidget extends StatefulWidget {
   final List<String> columnNames;
 
   DataTableWidget(this.columnNames, {Key? key}) : super(key: key);
+
+  //Create the initial state of the widget
+  //Which in this case sets the values for the dropdown
+  @override
+  State<StatefulWidget> createState() {
+    return _DataTableWidgetState();
+  }
+}
+
+class _DataTableWidgetState extends State<DataTableWidget> {
+  //A dictionary to store the role of each column in the dataset
+  //The key is the column name
+  //value is the column name
+  Map<String, String> dropdownValues = {};
 
   // A method to make rows from variable names
   // In a given row , the first element is the row name ,
@@ -11,33 +25,41 @@ class DataTableWidget extends StatelessWidget {
   //The third element is a drop down with 5 options {Input , Text , Risk , Ident , Weight}
   List<DataRow> makeVarNames(List<String> varNames) {
     return varNames.map((String varName) {
+      //Default to input if a role is not selected
+      dropdownValues.putIfAbsent(varName, () => 'Input');
       return DataRow(
         cells: [
           DataCell(Text(varName)),
           DataCell(const Text("Type")),
           DataCell(
             DropdownButton<String>(
-              value: "input",
+              value: dropdownValues[varName],
               items: [
                 DropdownMenuItem(
                   child: Text("Input"),
-                  value: 'input',
+                  value: 'Input',
                 ),
                 DropdownMenuItem(
                   child: Text("Target"),
-                  value: 'text',
+                  value: 'Target',
                 ),
                 DropdownMenuItem(
                   child: Text("Risk"),
-                  value: 'risk',
+                  value: 'Risk',
                 ),
                 DropdownMenuItem(
-                  child: Text("Ident"),
-                  value: 'weight',
+                  child: Text("Weight"),
+                  value: 'Weight',
                 ),
               ],
+              //Set the current value of the dropdown
+              //Use the setstate() method that comes with
+              //a stateful widget
               onChanged: (String? value) {
-                print(value);
+                setState(() {
+                  dropdownValues[varName] = value!;
+                  print(value);
+                });
               },
             ),
           ),
