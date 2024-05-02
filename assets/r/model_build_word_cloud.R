22e0b3937046fb014bded2bcec5af093a1d691cd
yixiangyin <y1027020239@qq.com>
2024-05-02 12:29:40 +1000

    add min freq function

--- assets/r/model_build_word_cloud.R
+++ assets/r/model_build_word_cloud.R
@@ -33,6 +33,6 @@ set.seed(123)
 png("WORDCLOUDPATH", width = 800, height = 600, units = "px")
 
 # Generate word cloud
-wordcloud(words = d$word, freq = d$freq, scale=c(5,0.5), min.freq = 1, max.word = MAXWORD, random.order = RANDOMORDER, colors=brewer.pal(8, "Dark2"))
+wordcloud(words = d$word, freq = d$freq, scale=c(5,0.5), min.freq = MINFREQ, max.word = MAXWORD, random.order = RANDOMORDER, colors=brewer.pal(8, "Dark2"))
 
 dev.off()
