331f388e03164e214ee541e5c1398a883ebf11fa
yixiangyin <y1027020239@qq.com>
2024-05-08 22:53:10 +1000

    working

---
+++ lib/features/model/forest_tab.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:rattle/constants/app.dart';
+import 'package:rattle/provider/stdout.dart';
+import 'package:rattle/r/extract_forest.dart';
+
+class ForestTab extends ConsumerWidget {
+  const ForestTab({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    String stdout = ref.watch(stdoutProvider);
+    return Expanded(
+      child: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.only(left: 10),
+        child: SingleChildScrollView(
+          child: SelectableText(
+            rExtractForest(stdout),
+            style: monoTextStyle,
+          ),
+        ),
+      ),
+    );
+  }
+}
