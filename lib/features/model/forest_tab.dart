274327fdfb6196bbedafe7eda7c0bf7b44e91194
yixiangyin <y1027020239@qq.com>
2024-05-13 23:22:44 +1000

    restore buttons for forest and tree tab

--- lib/features/model/forest_tab.dart
+++ lib/features/model/forest_tab.dart
@@ -3,22 +3,27 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:rattle/constants/app.dart';
 import 'package:rattle/provider/stdout.dart';
 import 'package:rattle/r/extract_forest.dart';
+import 'package:rattle/utils/add_build_button.dart';
 
 class ForestTab extends ConsumerWidget {
-  const ForestTab({super.key});
+  final Widget buildButton;
+  const ForestTab({super.key, required this.buildButton});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     String stdout = ref.watch(stdoutProvider);
-    return Container(
-      width: double.infinity,
-      padding: const EdgeInsets.only(left: 10),
-      child: SingleChildScrollView(
-        child: SelectableText(
-          rExtractForest(stdout),
-          style: monoTextStyle,
+    Widget content = Expanded(
+      child: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.only(left: 10),
+        child: SingleChildScrollView(
+          child: SelectableText(
+            rExtractForest(stdout),
+            style: monoTextStyle,
+          ),
         ),
       ),
     );
+    return addBuildButton(content, buildButton);
   }
 }
