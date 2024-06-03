b4f37b83191bf78d988b1952f074fd8d40209668
Graham Williams <graham.williams@togaware.com>
2024-06-04 05:35:35 +1000

    Improve make support.

--- support/flutter.mk
+++ support/flutter.mk
@@ -323,6 +323,7 @@ BRANCH := $(shell git branch --show-current)
 
 ifeq ($(BRANCH),dev)
 push::
+	@echo "-------------------------------------------------------"
 	perl -pi -e 's|(^version: .*)\+.*|$$1+$(VERSEQ)|' pubspec.yaml
 	-egrep '^version: .*\+.*' pubspec.yaml && \
 	git commit -m "Bump sequence $(VERSEQ)" pubspec.yaml
