22e06f4b0b1a5d0e47dc43f0e69191f13aa7064f
Graham Williams <graham.williams@togaware.com>
2024-06-04 06:40:23 +1000

    Be sure to push bump of sequence number

--- support/flutter.mk
+++ support/flutter.mk
@@ -320,6 +320,7 @@ push::
 	perl -pi -e 's|(^version: .*)\+.*|$$1+$(VERSEQ)|' pubspec.yaml
 	-egrep '^version: .*\+.*' pubspec.yaml && \
 	git commit -m "Bump sequence $(VERSEQ)" pubspec.yaml
+	git push
 endif
 
 .PHONY: publish
