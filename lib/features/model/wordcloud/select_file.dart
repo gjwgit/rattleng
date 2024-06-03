f1c461dc0b35714a461b4f3e08a42bd04433bbff
Graham Williams <graham.williams@togaware.com>
2024-06-04 06:55:36 +1000

    Note that file picker default filename not shown on linux.

--- lib/features/model/wordcloud/select_file.dart
+++ lib/features/model/wordcloud/select_file.dart
@@ -6,6 +6,8 @@ Future<String?> selectFile() async {
 
   String? result = await FilePicker.platform.saveFile(
     dialogTitle: "pick a location to save the file",
+    // TODO 20240604 gjw THE DEFAULT FILE NAME DOES NOT APPEAR IN THE DIALOG ON
+    // LINUX. COULD BE A PACKAGE BUG? SHOULD REPORT IF SO.
     fileName: "wordcloud.png",
   );
   return result;
