2efa3748446f0b46683007545907ebcfc5da95e7
yixiangyin <y1027020239@qq.com>
2024-06-07 00:38:02 +1000

    add license

--- lib/r/source.dart
+++ lib/r/source.dart
@@ -22,7 +22,7 @@
 // You should have received a copy of the GNU General Public License along with
 // this program.  If not, see <https://www.gnu.org/licenses/>.
 ///
-/// Authors: Graham Williams
+/// Authors: Graham Williams, Yixiang Yin
 library;
 
 import 'dart:convert';
