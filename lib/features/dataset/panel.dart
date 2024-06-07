590c6069ef9e9d83d5d12ca1c0cad1504a50896a
Graham Williams <graham.williams@togaware.com>
2024-06-07 13:59:51 +1000

    Restructure into a template for each feature: tab/config/panel

---
+++ lib/features/dataset/panel.dart
@@ -0,0 +1,74 @@
+/// Widget to display the Rattle introduction or view.
+///
+/// Copyright (C) 2023-2024, Togaware Pty Ltd.
+///
+/// License: GNU General Public License, Version 3 (the "License")
+/// https://www.gnu.org/licenses/gpl-3.0.en.html
+//
+// Time-stamp: <Friday 2024-06-07 13:50:54 +1000 Graham Williams>
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <https://www.gnu.org/licenses/>.
+///
+/// Authors: Graham Williams
+
+library;
+
+import 'package:flutter/material.dart';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:rattle/constants/app.dart';
+import 'package:rattle/constants/keys.dart';
+import 'package:rattle/provider/path.dart';
+import 'package:rattle/provider/stdout.dart';
+import 'package:rattle/r/extract_glimpse.dart';
+import 'package:rattle/widgets/markdown_file.dart';
+
+/// The dataset panel displayes the Rattle explainer or a data summary.
+
+class DatasetPanel extends ConsumerStatefulWidget {
+  const DatasetPanel({super.key});
+
+  @override
+  ConsumerState<DatasetPanel> createState() => _DatasetPanelState();
+}
+
+class _DatasetPanelState extends ConsumerState<DatasetPanel> {
+  @override
+  Widget build(BuildContext context) {
+    String path = ref.watch(pathProvider);
+    String stdout = ref.watch(stdoutProvider);
+
+    if (path == '') {
+      return Expanded(
+        child: Center(
+          key: welcomeTextKey,
+          child: sunkenMarkdownFileBuilder(welcomeMsgFile),
+        ),
+      );
+    } else {
+      return Expanded(
+        child: Container(
+          width: double.infinity,
+          padding: const EdgeInsets.only(left: 10),
+          child: SelectableText(
+            rExtractGlimpse(stdout),
+            key: datasetGlimpseKey,
+            style: monoTextStyle,
+          ),
+        ),
+      );
+    }
+  }
+}
