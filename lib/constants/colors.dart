aab9ad9f5da7a4860f375f1cab494b119bc0b3a5
Graham Williams <graham.williams@togaware.com>
2024-06-08 21:09:33 +1000

    Remove unused colors.

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -5,7 +5,7 @@
 /// License: https://www.gnu.org/licenses/gpl-3.0.en.html
 ///
 //
-// Time-stamp: <Saturday 2024-06-08 20:32:46 +1000 Graham Williams>
+// Time-stamp: <Saturday 2024-06-08 21:08:08 +1000 Graham Williams>
 //
 // Licensed under the GNU General Public License, Version 3 (the "License");
 ///
@@ -27,38 +27,6 @@
 library;
 
 import 'package:flutter/material.dart';
-
-// Colour Options
-
-// Color(0xff45035e) is a solid dark purple, suitable for white text. This
-// standout colour could be the background used throughout the app.
-//
-// Color(0x5545035e) is a medium purple, suitable for black text. This can be a
-// softer contrast to a darker purple as might be used for the tab areas at the
-// top of the app.
-
-// Color(0xfffef7ff) is a very pale (too pale) purple that looks somewhat washed
-// out.
-
-/// The default background colour of the header and side bar.
-
-const headerBarColor = Colors.red; //Color(0xff45035e);
-
-/// The default background colour of the tab bars in the header and side bars.
-
-const tabBarColor = Colors.red; //Color(0xff45035e);
-
-/// The default background colour for the configuration bars in each panel.
-
-const configBarColor = Colors.red;
-
-/// The default background colour for the configuration bars in each panel.
-
-const panelBarColor = Colors.red; //Color(0xfffef7ff);
-
-/// The default background colour of the status bar at the bottom of the app.
-
-final statusBarColor = Colors.deepPurple.shade100;
 
 /// A [BoxDecoration] with a sunken effect for use with a [Container]'s
 /// decortation.
