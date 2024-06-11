39e6d675a109b525cae113a2b72111dd76c4e3b0
Graham Williams <graham.williams@togaware.com>
2024-06-11 11:22:04 +1000

    Add sample R code for summary.

--- assets/r/explore_summary.R
+++ assets/r/explore_summary.R
@@ -25,6 +25,22 @@ library(Hmisc, quietly=TRUE)
 
 describe(crs$dataset[crs$train, c(crs$input, crs$risk, crs$target)])
 
+# The 'basicStats' package provides the 'fBasics' function.
+
+library(fBasics, quietly=TRUE)
+
+# Generate a description of the numeric data.
+
+lapply(crs$dataset[crs$train, c(crs$input, crs$risk, crs$target)][,c(1:5, 7, 10:19, 21)], basicStats)
+
+# The 'kurtosis' package provides the 'fBasics' function.
+
+library(fBasics, quietly=TRUE)
+
+# Summarise the kurtosis of the numeric data.
+
+kurtosis(crs$dataset[crs$train, c(crs$input, crs$risk, crs$target)][,c(1:5, 7, 10:19, 21)], na.rm=TRUE)
+
 # The 'skewness' package provides the 'fBasics' function.
 
 library(fBasics, quietly=TRUE)
@@ -40,3 +56,18 @@ library(mice, quietly=TRUE)
 # Generate a summary of the missing values in the dataset.
 
 md.pattern(crs$dataset[,c(crs$input, crs$target)])
+
+# The 'CrossTable' package provides the 'descr' function.
+
+library(descr, quietly=TRUE)
+
+# Generate cross tabulations for categoric data.
+
+for (i in c(8, 10:11, 22)) 
+{ 
+  cat(sprintf('CrossTab of %s by target variable %s\n\n', names(crs$dataset)[i], crs$target)) 
+  print(CrossTable(crs$dataset[[i]], crs$dataset[[crs$target]], expected=TRUE, format='SAS')) 
+  cat(paste(rep('=', 70), collapse=''), '
+
+') 
+}
