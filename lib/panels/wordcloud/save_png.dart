dd0c77e9f0d1eb579e5041496d146cdd07c22219
yixiangyin <y1027020239@qq.com>
2024-06-19 18:13:39 +1000

    lint update

--- lib/panels/wordcloud/save_png.dart
+++ lib/panels/wordcloud/save_png.dart
@@ -27,29 +27,29 @@ library;
 
 // Group imports by dart, flutter, packages, local. Then alphabetically.
 
-import 'dart:io';
+// import 'dart:io';
 
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-import 'package:rattle/panels/wordcloud/select_file.dart';
+// import 'package:rattle/panels/wordcloud/select_file.dart';
 
-class WordCloudSaveButton extends StatelessWidget {
-  // Path to the word cloud temp image file.
-  final String wordCloudImagePath;
+// class WordCloudSaveButton extends StatelessWidget {
+//   // Path to the word cloud temp image file.
+//   final String wordCloudImagePath;
 
-  const WordCloudSaveButton({super.key, required this.wordCloudImagePath});
+//   const WordCloudSaveButton({super.key, required this.wordCloudImagePath});
 
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () async {
-        String? pathToSave = await selectFile();
-        if (pathToSave != null) {
-          // Copy generated image from /tmp to user's location.
-          await File(wordCloudImagePath).copy(pathToSave);
-        }
-      },
-      child: const Text('Save Word Cloud Image'),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return ElevatedButton(
+//       onPressed: () async {
+//         String? pathToSave = await selectFile();
+//         if (pathToSave != null) {
+//           // Copy generated image from /tmp to user's location.
+//           await File(wordCloudImagePath).copy(pathToSave);
+//         }
+//       },
+//       child: const Text('Save Word Cloud Image'),
+//     );
+//   }
+// }
