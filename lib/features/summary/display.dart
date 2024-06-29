12157563427ba754d7d1747810db4ee94b3e4d33
Graham Williams <graham.williams@togaware.com>
2024-06-29 15:45:09 +1000

    Bug fix - remove empty page from Summary.

--- lib/features/summary/display.dart
+++ lib/features/summary/display.dart
@@ -5,7 +5,7 @@
 /// License: GNU General Public License, Version 3 (the "License")
 /// https://www.gnu.org/licenses/gpl-3.0.en.html
 //
-// Time-stamp: <Saturday 2024-06-29 15:19:53 +1000 Graham Williams>
+// Time-stamp: <Saturday 2024-06-29 15:33:58 +1000 Graham Williams>
 //
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
@@ -189,7 +189,7 @@ class _SummaryDisplayState extends ConsumerState<SummaryDisplay> {
 
     content = lines.join('\n');
 
-    if (content.isNotEmpty) {
+    if (lines.length > 4) {
       pages.add(
         TextPage(
           title: '# Kurtosis and Skewness\n\n'
