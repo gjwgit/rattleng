24c6ec98c3be1e729da9a52698ee98794cf32ab8
Graham Williams <graham.williams@togaware.com>
2024-07-20 14:42:03 +1000

    Add missing impute

---
+++ assets/r/transform_impute_constant.R
@@ -0,0 +1,34 @@
+# Rattle Scripts: Data Transformation/Wrangling
+#
+# Copyright (C) 2024, Togaware Pty Ltd.
+#
+# License: GNU General Public License, Version 3 (the "License")
+# https://www.gnu.org/licenses/gpl-3.0.en.html
+#
+# Time-stamp: <Saturday 2024-07-20 10:54:53 +1000 Graham Williams>
+#
+# Licensed under the GNU General Public License, Version 3 (the "License");
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Author: Graham Williams
+
+# TODO 20240720 gjw combine into impute_constant
+
+# Transform "SELECTED_VAR" into "IMP_SELECTED_VAR" by imputing a value.
+
+ds %<>%
+  mutate(IMP_SELECTED_VAR = ifelse(is.na(SELECTED_VAR),
+                                   IMPUTED_VALUE,
+                                   SELECTED_VAR))
