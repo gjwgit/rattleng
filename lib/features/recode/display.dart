6d829acb5eb4de0b0e5ccb9d76ae9368bf8e38d9
Graham Williams <graham.williams@togaware.com>
2024-07-23 08:12:51 +1000

    Add glimpse to each transform tab feature.

--- lib/features/recode/display.dart
+++ lib/features/recode/display.dart
@@ -5,7 +5,7 @@
 /// License: GNU General Public License, Version 3 (the "License")
 /// https://www.gnu.org/licenses/gpl-3.0.en.html
 //
-// Time-stamp: <Friday 2024-07-19 09:41:09 +1000 Graham Williams>
+// Time-stamp: <Tuesday 2024-07-23 06:59:55 +1000 Graham Williams>
 //
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
@@ -28,12 +28,12 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'package:rattle/constants/app.dart';
 import 'package:rattle/constants/markdown.dart';
-import 'package:rattle/constants/sunken_box_decoration.dart';
 import 'package:rattle/providers/stdout.dart';
-import 'package:rattle/r/extract_empty.dart';
+import 'package:rattle/r/extract_glimpse.dart';
+import 'package:rattle/widgets/pages.dart';
 import 'package:rattle/widgets/show_markdown_file.dart';
+import 'package:rattle/widgets/text_page.dart';
 
 /// The panel displays the instructions or the output.
 
@@ -48,22 +48,24 @@ class _RecodeDisplayState extends ConsumerState<RecodeDisplay> {
   @override
   Widget build(BuildContext context) {
     String stdout = ref.watch(stdoutProvider);
-    String content = rExtractEmpty(stdout);
 
-    return content == ''
-        ? showMarkdownFile(recodeIntroFile, context)
-        : Expanded(
-            child: Container(
-              decoration: sunkenBoxDecoration,
-              width: double.infinity,
-              padding: const EdgeInsets.only(left: 10),
-              child: SingleChildScrollView(
-                child: SelectableText(
-                  content,
-                  style: monoTextStyle,
-                ),
-              ),
-            ),
-          );
+    List<Widget> pages = [showMarkdownFile(recodeIntroFile, context)];
+
+    // Second page is the data summary.
+
+    String content = rExtractGlimpse(stdout);
+
+    if (content.isNotEmpty) {
+      pages.add(
+        TextPage(
+          title: '# Dataset Glimpse\n\nGenerated using `glimpse(ds)`',
+          content: '\n$content',
+        ),
+      );
+    }
+
+    ////////////////////////////////////////////////////////////////////////
+
+    return Pages(children: pages);
   }
 }
