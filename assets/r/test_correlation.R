ceb0973324e410032ac32655af5defb16e1339e9
Graham Williams <graham.williams@togaware.com>
2024-07-24 05:44:07 +1000

    Bug fix.

--- assets/r/test_correlation.R
+++ assets/r/test_correlation.R
@@ -6,8 +6,10 @@ library(fBasics, quietly=TRUE)
 
 # Perform the test.
 
-miss <- union(attr(na.omit(ds[,"SELECTED_VAR"]), "na.action"),
-              attr(na.omit(ds[,"SELECTED2_VAR"]), "na.action"))
+#miss <- union(attr(na.omit(ds[,"SELECTED_VAR"]), "na.action"),
+#              attr(na.omit(ds[,"SELECTED2_VAR"]), "na.action"))
 
-correlationTest(na.omit(ds[-miss, "SELECTED_VAR"]),
-                na.omit(ds[-miss, "SELECTED2_VAR"]))
+#correlationTest(na.omit(ds[-miss, "SELECTED_VAR"]),
+#                na.omit(ds[-miss, "SELECTED2_VAR"]))
+
+correlationTest(ds$SELECTED_VAR, ds$SELECTED2_VAR)
