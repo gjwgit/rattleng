c88ac3807873f540b168649ec01894e018d0452d
Graham Williams <graham.williams@togaware.com>
2024-07-29 08:46:37 +1000

    Bug fix.

--- lib/features/visual/display.dart
+++ lib/features/visual/display.dart
@@ -5,7 +5,7 @@
 /// License: GNU General Public License, Version 3 (the "License")
 /// https://www.gnu.org/licenses/gpl-3.0.en.html
 //
-// Time-stamp: <Monday 2024-07-29 08:21:38 +1000 Graham Williams>
+// Time-stamp: <Monday 2024-07-29 08:46:18 +1000 Graham Williams>
 //
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
@@ -77,27 +77,45 @@ class _VisualDisplayState extends ConsumerState<VisualDisplay> {
 
       pages.add(
         ImagePage(
-          title: '# Density Plot of Values\n\n'
-              'Generated using [ggplot2::geom_density()]'
-              '(https://www.rdocumentation.org/packages/ggplot2/topics/geom_density).',
+          title: '''
+
+          # Density Plot of Values
+
+          Visit the [Survival
+          Guide](https://survivor.togaware.com/datascience/density-plot.html) and
+          [ggplot2::geom_density()](https://www.rdocumentation.org/packages/ggplot2/topics/geom_density).
+
+          ''',
           path: '$tempDir/explore_visual_density.svg',
         ),
       );
 
       pages.add(
         ImagePage(
-          title: '# Cummulative Plot\n\n'
-              'Generated using [ggplot2::stat_ecdf()]'
-              '(https://www.rdocumentation.org/packages/ggplot2/topics/stat_ecdf).',
+          title: '''
+
+          # Cumulative Plot
+
+          Visit the [Survival
+          Guide](https://survivor.togaware.com/datascience/cumulative-plot.html) and
+          [ggplot2::stat_ecdf()](https://www.rdocumentation.org/packages/ggplot2/topics/stat_ecdf).
+
+          ''',
           path: '$tempDir/explore_visual_ecdf.svg',
         ),
       );
 
       pages.add(
         ImagePage(
-          title: '# Benford Plot\n\n'
-              'Generated using [rattle::plotDigitFreq()]'
-              '(https://www.rdocumentation.org/packages/rattle).',
+          title: '''
+
+          # Benford Plot
+
+          Visit the [Survival
+          Guide](https://survivor.togaware.com/datascience/benford-plot.html) and
+          [rattle::plotDigitFreq()](https://www.rdocumentation.org/packages/rattle).
+
+          ''',
           path: '$tempDir/explore_visual_benford.svg',
         ),
       );
@@ -117,27 +135,45 @@ class _VisualDisplayState extends ConsumerState<VisualDisplay> {
     if (!numeric) {
       pages.add(
         ImagePage(
-          title: '# Bar Chart\n\n'
-              'Generated using [gplots::barplot2()]'
-              '(https://www.rdocumentation.org/packages/gplots/topics/barplot2).',
+          title: '''
+
+          # Bar Chart
+
+          Visit the [Survival
+          Guide](https://survivor.togaware.com/datascience/bar-chart.html) and
+          [gplots::barplot2()](https://www.rdocumentation.org/packages/gplots/topics/barplot2).
+
+          ''',
           path: '$tempDir/explore_visual_bars.svg',
         ),
       );
 
       pages.add(
         ImagePage(
-          title: '# Dot Plot\n\n'
-              'Generated using [graphics::dotchart()]'
-              '(https://www.rdocumentation.org/packages/graphics/topics/dotchart).',
+          title: '''
+
+          # Dot Plot
+
+          Visit the [Survival
+          Guide](https://survivor.togaware.com/datascience/dot-plot.html) and
+          [graphics::dotchart()](https://www.rdocumentation.org/packages/graphics/topics/dotchart).
+
+          ''',
           path: '$tempDir/explore_visual_dots.svg',
         ),
       );
 
       pages.add(
         ImagePage(
-          title: '# Pairs Plot\n\n'
-              'Generated using [graphics::mosaicplot()]'
-              '(https://www.rdocumentation.org/packages/graphics/topics/mosaicplot).',
+          title: '''
+
+          # Mosaic Plot
+
+          Visit the [Survival
+          Guide](https://survivor.togaware.com/datascience/mosaic-plot.html) and
+          [graphics::mosaicplot()](https://www.rdocumentation.org/packages/graphics/topics/mosaicplot).
+
+          ''',
           path: '$tempDir/explore_visual_mosaic.svg',
         ),
       );
