2509a70876103617f8b7134aa714e9f7842ccff9
Graham Williams <graham.williams@togaware.com>
2024-07-29 08:23:08 +1000

    Need to use Pages not just a single page for TESTS

--- lib/features/tests/display.dart
+++ lib/features/tests/display.dart
@@ -5,7 +5,7 @@
 /// License: GNU General Public License, Version 3 (the "License")
 /// https://www.gnu.org/licenses/gpl-3.0.en.html
 //
-// Time-stamp: <Friday 2024-06-14 14:30:08 +1000 Graham Williams>
+// Time-stamp: <Monday 2024-07-29 08:22:52 +1000 Graham Williams>
 //
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
@@ -28,12 +28,12 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'package:rattle/constants/app.dart';
 import 'package:rattle/constants/markdown.dart';
-import 'package:rattle/constants/sunken_box_decoration.dart';
 import 'package:rattle/providers/stdout.dart';
-import 'package:rattle/r/extract_tests.dart';
+import 'package:rattle/r/extract.dart';
+import 'package:rattle/widgets/pages.dart';
 import 'package:rattle/widgets/show_markdown_file.dart';
+import 'package:rattle/widgets/text_page.dart';
 
 /// The panel displays the instructions or the output.
 
@@ -45,23 +45,134 @@ class TestsDisplay extends ConsumerStatefulWidget {
 }
 
 class _TestsDisplayState extends ConsumerState<TestsDisplay> {
+  //
+
   @override
   Widget build(BuildContext context) {
     String stdout = ref.watch(stdoutProvider);
-    String content = rExtractTests(stdout);
 
-    return content == ''
-        ? showMarkdownFile(testsIntroFile, context)
-        : Container(
-            decoration: sunkenBoxDecoration,
-            width: double.infinity,
-            padding: const EdgeInsets.only(left: 10),
-            child: SingleChildScrollView(
-              child: SelectableText(
-                content,
-                style: monoTextStyle,
-              ),
-            ),
-          );
+    List<Widget> pages = [showMarkdownFile(testsIntroFile, context)];
+
+    String content = '';
+
+    ////////////////////////////////////////////////////////////////////////
+    // CORRELATION
+    ////////////////////////////////////////////////////////////////////////
+
+    content = rExtract(stdout, '> correlationTest(');
+
+    if (content.isNotEmpty) {
+      pages.add(
+        TextPage(
+          title: '''
+
+          # Pearson Correlation Test
+
+          Visit the [Survival
+          Guide](https://survivor.togaware.com/datascience/correlation-test.html)
+          and
+          [fBasics::correlationTest()](https://www.rdocumentation.org/packages/fBasics/topics/correlationTest)
+
+            ''',
+          content: content,
+        ),
+      );
+    }
+
+    ////////////////////////////////////////////////////////////////////////
+    // KOLMOGOROV-SMIRNOV TWO SAMPLE TEST
+    ////////////////////////////////////////////////////////////////////////
+
+    content = rExtract(stdout, '> ks2Test(');
+
+    if (content.isNotEmpty) {
+      pages.add(
+        TextPage(
+          title: '''
+
+          # Kolmogorov-Smirnov Two Sample Test
+
+          Visit the [Survival
+          Guide](https://survivor.togaware.com/datascience/kolmogorov-smirnov-test.html)
+          and
+          [fBasics::ks2Test()](https://www.rdocumentation.org/packages/fBasics/topics/ks2Test)
+
+            ''',
+          content: content,
+        ),
+      );
+    }
+
+    ////////////////////////////////////////////////////////////////////////
+    // WILCOX
+    ////////////////////////////////////////////////////////////////////////
+
+    content = rExtract(stdout, '> wilcox.test(');
+
+    if (content.isNotEmpty) {
+      pages.add(
+        TextPage(
+          title: '''
+
+          # Wilcoxon Rank Sum Test
+
+          Visit the [Survival Guide](https://survivor.togaware.com/datascience/wilcoxon-rank-sum-test.html) and
+          [stats::wilcoxon.test()](https://www.rdocumentation.org/packages/stats/topics/wilcox.test)
+
+            ''',
+          content: content,
+        ),
+      );
+    }
+
+    ////////////////////////////////////////////////////////////////////////
+    // CORRELATION
+    ////////////////////////////////////////////////////////////////////////
+
+    content = rExtract(stdout, '> locationTest(');
+
+    if (content.isNotEmpty) {
+      pages.add(
+        TextPage(
+          title: '''
+
+          # t-Test Two Sample Location Test
+
+          Visit the [Survival Guide](https://survivor.togaware.com/datascience/t-test-two-sample.html) and
+          [fBasics::locationTest()](https://www.rdocumentation.org/packages/fBasics/topics/locationTest)
+
+            ''',
+          content: content,
+        ),
+      );
+    }
+
+    ////////////////////////////////////////////////////////////////////////
+    // CORRELATION
+    ////////////////////////////////////////////////////////////////////////
+
+    content = rExtract(stdout, '> varianceTest');
+
+    if (content.isNotEmpty) {
+      pages.add(
+        TextPage(
+          title: '''
+
+          # Two Sample Variance Test
+
+          Visit the [Survival
+          Guide](https://survivor.togaware.com/datascience/f-test-two-sample.html)
+          and
+          [fBasics::varianceTest()](https://www.rdocumentation.org/packages/fBasics/topics/varianceTest)
+
+            ''',
+          content: content,
+        ),
+      );
+    }
+
+    ////////////////////////////////////////////////////////////////////////
+
+    return Pages(children: pages);
   }
 }
