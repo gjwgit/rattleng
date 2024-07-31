6c93d7c79215be9877c812d3bcecae21c6fa6b8e
yixiangyin <y1027020239@qq.com>
2024-07-31 16:15:23 +1000

    add support for Impute

--- lib/utils/update_roles_provider.dart
+++ lib/utils/update_roles_provider.dart
@@ -3,19 +3,38 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:rattle/providers/roles.dart';
 import 'package:rattle/r/extract_vars.dart';
 
+Set<String> transformPrefix = {
+// rescale
+  'RRC',
+  'R01',
+  'RMD',
+  'RLG',
+  'R10',
+// Impute
+  'IZR',
+  'IMN',
+  'IMD',
+  'IMO',
+  'IMP',
+};
+
+bool isTransformedVar(String name) {
+  for (var prefix in transformPrefix) {
+    if (name.startsWith('${prefix}_')) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
 void updateRolesProvider(List<VariableInfo> vars, WidgetRef ref) {
   // When a new row is added after transformation, initialise its role and update the role of the old variable
   for (var column in vars) {
     if (!ref.read(rolesProvider.notifier).state.containsKey(column.name)) {
-      if (column.name.startsWith('RRC_') ||
-          column.name.startsWith('R01_') ||
-          column.name.startsWith('RMD_') ||
-          column.name.startsWith('RLG_') ||
-          column.name.startsWith('R10_')) {
+      if (isTransformedVar(column.name)) {
         ref.read(rolesProvider.notifier).state[column.name] = Role.input;
-        ref
-                .read(rolesProvider.notifier)
-                .state[column.name.substring(4)] =
+        ref.read(rolesProvider.notifier).state[column.name.substring(4)] =
             Role.ignoreAfterTransformed;
       } else {
         debugPrint('uninitialised new variables!');
