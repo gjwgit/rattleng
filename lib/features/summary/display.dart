e2bac86dcbbde0e1fd02912e864c61be79220bd0
Graham Williams <graham.williams@togaware.com>
2024-07-31 21:26:47 +1000

    Bug fix for SUMMARY.

--- lib/features/summary/display.dart
+++ lib/features/summary/display.dart
@@ -5,7 +5,7 @@
 /// License: GNU General Public License, Version 3 (the "License")
 /// https://www.gnu.org/licenses/gpl-3.0.en.html
 //
-// Time-stamp: <Wednesday 2024-07-31 05:59:04 +1000 Graham Williams>
+// Time-stamp: <Wednesday 2024-07-31 21:25:27 +1000 Graham Williams>
 //
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
@@ -224,7 +224,7 @@ class _SummaryDisplayState extends ConsumerState<SummaryDisplay> {
     // FBASICS STATS
     ////////////////////////////////////////////////////////////////////////
 
-    content = rExtract(stdout, 'lapply(ds[numc], basicStats)');
+    content = rExtract(stdout, 'lapply(ds[numc], fBasics::basicStats)');
 
     // Replace $ at the beginning of any lines.
 
