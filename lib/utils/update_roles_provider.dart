261659f4471007565b0d7f55b87fb08ddf50988d
yixiangyin <y1027020239@qq.com>
2024-07-31 16:06:14 +1000

    support for rescale other than recenter

--- lib/utils/update_roles_provider.dart
+++ lib/utils/update_roles_provider.dart
@@ -7,11 +7,16 @@ void updateRolesProvider(List<VariableInfo> vars, WidgetRef ref) {
   // When a new row is added after transformation, initialise its role and update the role of the old variable
   for (var column in vars) {
     if (!ref.read(rolesProvider.notifier).state.containsKey(column.name)) {
-      if (column.name.startsWith('RRC_')) {
+      if (column.name.startsWith('RRC_') ||
+          column.name.startsWith('R01_') ||
+          column.name.startsWith('RMD_') ||
+          column.name.startsWith('RLG_') ||
+          column.name.startsWith('R10_')) {
         ref.read(rolesProvider.notifier).state[column.name] = Role.input;
         ref
-            .read(rolesProvider.notifier)
-            .state[column.name.replaceFirst('RRC_', '')] = Role.ignoreAfterTransformed;
+                .read(rolesProvider.notifier)
+                .state[column.name.substring(4)] =
+            Role.ignoreAfterTransformed;
       } else {
         debugPrint('uninitialised new variables!');
       }
