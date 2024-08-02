ac0df382bc0eaa1e9f896a395496f31dd4705e38
yixiangyin <y1027020239@qq.com>
2024-08-02 22:10:48 +1000

    update script

--- assets/r/transform_rescale_interval.R
+++ assets/r/transform_rescale_interval.R
@@ -4,9 +4,12 @@
 
 # Rescale SELECTED_VAR.
 
-ds[["RIN_SELECTED_VAR_INTERVALS"]] <- ds[["SELECTED_VAR"]]
+ds[["RIN_SELECTED_VAR_INTERVAL"]] <- ds[["SELECTED_VAR"]]
 
-# Rescale to 0 to INTERVALS within each group.
+# Rescale to 0 to INTERVAL within each group.
 
 ds[["RIN_SELECTED_VAR_INTERVAL"]] <-
-    reshape::rescale.by.group(ds[["SELECTED_VAR"]], type="irank", itop=INTERVALS)
+    reshape::rescale.by.group(ds[["SELECTED_VAR"]], type="irank", itop=INTERVAL)
+
+glimpse(ds)
+summary(ds)
