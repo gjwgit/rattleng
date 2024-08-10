6d6a8fab097d37f79272a6b72729840fead97e0d
yixiangyin <y1027020239@qq.com>
2024-08-10 18:32:30 +1000

    gui

--- lib/features/recode/config.dart
+++ lib/features/recode/config.dart
@@ -25,15 +25,19 @@
 library;
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:rattle/constants/spacing.dart';
+import 'package:rattle/providers/number.dart';
 import 'package:rattle/providers/selected.dart';
 import 'package:rattle/utils/get_inputs.dart';
 
 import 'package:rattle/utils/show_under_construction.dart';
 import 'package:rattle/utils/variable_chooser.dart';
 import 'package:rattle/widgets/activity_button.dart';
+import 'package:rattle/widgets/custom_choice_chip.dart';
+import 'package:rattle/widgets/number_field.dart';
 
 /// The SVM tab config currently consists of just an ACTIVITY button.
 ///
@@ -47,6 +51,66 @@ class RecodeConfig extends ConsumerStatefulWidget {
 }
 
 class RecodeConfigState extends ConsumerState<RecodeConfig> {
+  String selectedTransform = 'Quantiles';
+
+  List<String> numericMethods = [
+    'Quantiles',
+    'KMeans',
+    'Equal Width',
+  ];
+
+  List<String> categoricMethods = [
+    'Quantiles',
+    'KMeans',
+    'Equal Width',
+  ];
+
+  Widget recodeChooser() {
+    final TextEditingController valCtrl = TextEditingController();
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        Wrap(
+          spacing: 5.0,
+          children: numericMethods.map((transform) {
+            return CustomChoiceChip(
+              label: transform,
+              selectedTransform: selectedTransform,
+              onSelected: (bool selected) {
+                setState(() {
+                  selectedTransform = selected ? transform : '';
+                });
+              },
+            );
+          }).toList(),
+        ),
+        configWidgetSpace,
+        NumberField(
+          label: 'Number',
+          controller: valCtrl,
+          stateProvider: numberProvider,
+          validator: (value) => validateInteger(value, min: 1),
+          inputFormatter: FilteringTextInputFormatter.digitsOnly,
+        ),
+        configWidgetSpace,
+        Wrap(
+          spacing: 5.0,
+          children: categoricMethods.map((transform) {
+            return CustomChoiceChip(
+              label: transform,
+              selectedTransform: selectedTransform,
+              onSelected: (bool selected) {
+                setState(() {
+                  selectedTransform = selected ? transform : '';
+                });
+              },
+            );
+          }).toList(),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     List<String> inputs = getInputsAndIgnoreTransformed(ref);
@@ -79,6 +143,7 @@ class RecodeConfigState extends ConsumerState<RecodeConfig> {
             variableChooser(inputs, selected, ref),
           ],
         ),
+        recodeChooser(),
       ],
     );
   }
