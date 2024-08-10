bb7027940b208a4213a6cc3212b5dd790ca7ed44
Graham Williams <graham.williams@togaware.com>
2024-08-10 18:06:26 +1000

    Remove redundant string cleaner.

--- lib/r/extract_summary.dart
+++ lib/r/extract_summary.dart
@@ -5,7 +5,7 @@
 /// License: GNU General Public License, Version 3 (the "License")
 /// https://www.gnu.org/licenses/gpl-3.0.en.html
 //
-// Time-stamp: <Saturday 2024-08-10 14:39:53 +1000 Graham Williams>
+// Time-stamp: <Saturday 2024-08-10 17:51:29 +1000 Graham Williams>
 //
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
@@ -26,7 +26,7 @@ library;
 
 import 'package:rattle/r/extract.dart';
 
-import 'remove_windows_chars.dart';
+import 'package:rattle/utils/clean_string.dart';
 
 String rExtractSummary(String txt) {
   String content = rExtract(txt, 'summary(ds)');
@@ -49,7 +49,7 @@ String rExtractSummary(String txt) {
 
   // Clean the result.
 
-  content = removeWindowsChars(content);
+  content = cleanString(content);
 
   return content;
 }
