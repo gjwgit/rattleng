3201396f6fa9e003ff15931402e8e79d3443f002
Graham Williams <graham.williams@togaware.com>
2024-08-11 19:12:03 +1000

    Rewrite CLEANUP.

--- lib/features/cleanup/display.dart
+++ lib/features/cleanup/display.dart
@@ -5,7 +5,7 @@
 /// License: GNU General Public License, Version 3 (the "License")
 /// https://www.gnu.org/licenses/gpl-3.0.en.html
 //
-// Time-stamp: <Tuesday 2024-07-23 08:11:56 +1000 Graham Williams>
+// Time-stamp: <Sunday 2024-08-11 12:33:13 +1000 Graham Williams>
 //
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
@@ -30,7 +30,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:rattle/constants/markdown.dart';
 import 'package:rattle/providers/stdout.dart';
-import 'package:rattle/r/extract_glimpse.dart';
+import 'package:rattle/r/extract_summary.dart';
 import 'package:rattle/widgets/pages.dart';
 import 'package:rattle/widgets/show_markdown_file.dart';
 import 'package:rattle/widgets/text_page.dart';
@@ -53,12 +53,12 @@ class _CleanupDisplayState extends ConsumerState<CleanupDisplay> {
 
     // Second page is the data summary.
 
-    String content = rExtractGlimpse(stdout);
+    String content = rExtractSummary(stdout);
 
     if (content.isNotEmpty) {
       pages.add(
         TextPage(
-          title: '# Dataset Glimpse\n\nGenerated using `glimpse(ds)`',
+          title: '# Dataset Summary\n\nGenerated using `base::summary(ds)`',
           content: '\n$content',
         ),
       );
