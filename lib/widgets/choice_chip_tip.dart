d8ffc64cc4642ced0a45d24763b9d65b09006a7e
yixiangyin <y1027020239@qq.com>
2024-08-14 20:55:40 +1000

    author

--- lib/widgets/choice_chip_tip.dart
+++ lib/widgets/choice_chip_tip.dart
@@ -21,7 +21,7 @@
 // You should have received a copy of the GNU General Public License along with
 // this program.  If not, see <https://www.gnu.org/licenses/>.
 ///
-/// Authors: Graham Williams, Zheyuan Xu
+/// Authors: Graham Williams, Zheyuan Xu, Yixiang Yin
 
 library;
 
@@ -54,7 +54,7 @@ class ChoiceChipTip<T> extends StatelessWidget {
       runSpacing: choiceChipRowSpace,
       children: options.map((option) {
         final label = getLabel(option);
-        
+
         return ChoiceChip(
           label: Text(label),
           tooltip: tooltips == null ? '' : wordWrap(tooltips![option] ?? ''),
