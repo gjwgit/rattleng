f07ee43370ec8bcb9a8c7752b84d441f7cef3a51
Graham Williams <graham.williams@togaware.com>
2024-08-15 09:00:02 +1000

    Tidy up debugPrint for meta data.

--- lib/utils/update_meta_data.dart
+++ lib/utils/update_meta_data.dart
@@ -6,11 +6,9 @@ import 'package:rattle/providers/stdout.dart';
 import 'package:rattle/r/extract.dart';
 
 void updateMetaData(WidgetRef ref) {
-  debugPrint('UPDATE MEAT DATA');
-
   String stdout = ref.watch(stdoutProvider);
 
   String content = rExtract(stdout, '> meta_data(ds)');
 
-  print(content);
+  debugPrint('META DATA:\nXXXXXXXXXX\n$content\nXXXXXXXXXX');
 }
