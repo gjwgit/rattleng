7f55d6304f73b717c3831c50af726c6e3f4f62fa
yixiangyin <y1027020239@qq.com>
2024-08-15 00:29:35 +1000

    fix for 317

--- lib/utils/update_roles_provider.dart
+++ lib/utils/update_roles_provider.dart
@@ -6,6 +6,14 @@ import 'package:rattle/providers/vars/types.dart';
 import 'package:rattle/r/extract_vars.dart';
 import 'package:rattle/utils/is_numeric.dart';
 
+// Define the prefixes that need special handling. They have an number at suffix
+Set<String> specialPrefixes = {
+  'RIN',
+  'BK',
+  'BQ',
+  'BE',
+};
+
 Set<String> transformPrefix = {
 // rescale
   'RRC',
@@ -33,6 +41,37 @@ bool isTransformedVar(String name) {
   return false;
 }
 
+String getOriginal(String input) {
+    // It should return the var name before the transformation given the var name after the transformation
+  // Define the prefixes that need special handling
+  final List<String> specialPrefixes = ['RIN', 'BK', 'BQ', 'BE'];
+
+  // Find the index of the first underscore
+  int firstUnderscoreIndex = input.indexOf('_');
+
+  // If no underscore is found, return the original string
+  if (firstUnderscoreIndex == -1) {
+    return input;
+  }
+
+  // Extract the prefix from the input string
+  String prefix = input.substring(0, firstUnderscoreIndex);
+
+  // Remove everything before and including the first underscore
+  String result = input.substring(firstUnderscoreIndex + 1);
+
+  // If the prefix is in the list of special prefixes, handle the suffix
+  if (specialPrefixes.contains(prefix)) {
+    int lastUnderscoreIndex = result.lastIndexOf('_');
+    if (lastUnderscoreIndex != -1) {
+      // Remove the last underscore and everything after it
+      result = result.substring(0, lastUnderscoreIndex);
+    }
+  }
+
+  return result;
+}
+
 void updateVariablesProvider(WidgetRef ref) {
   // reset the rolesProvider and typesProvider
   // ref.read(rolesProvider.notifier).state = {};
@@ -46,7 +85,7 @@ void updateVariablesProvider(WidgetRef ref) {
     if (!ref.read(rolesProvider.notifier).state.containsKey(column.name)) {
       if (isTransformedVar(column.name)) {
         ref.read(rolesProvider.notifier).state[column.name] = Role.input;
-        ref.read(rolesProvider.notifier).state[column.name.substring(4)] =
+        ref.read(rolesProvider.notifier).state[getOriginal(column.name)] =
             Role.ignoreAfterTransformed;
       } else {
         debugPrint('ERROR: uninitialised new variables: ${column.name}!');
