b47b70c44a66e45c9a55e10c659f6bd7089acfd8
Graham Williams <graham.williams@togaware.com>
2024-08-20 07:06:41 +1000

    Split tests into features.

---
+++ integration_test/explore_test.dart
@@ -0,0 +1,201 @@
+/// Testing: Test the EXPLORE tab.
+//
+// Time-stamp: <Tuesday 2024-08-20 06:30:37 +1000 Graham Williams>
+//
+/// Copyright (C) 2023-2024, Togaware Pty Ltd
+///
+/// Licensed under the GNU General Public License, Version 3 (the "License");
+///
+/// License: https://www.gnu.org/licenses/gpl-3.0.en.html
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <https://www.gnu.org/licenses/>.
+///
+/// Authors: Graham Williams, Kevin Wang
+
+library;
+
+// Group imports by dart, flutter, packages, local. Then alphabetically.
+
+// TODO 20231015 gjw MIGRATE TESTS TO SINGLE ONE APP INSTANCE
+//
+// This will avoid a costly build each individual test? But then it is not so
+// well strctured.
+
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:integration_test/integration_test.dart';
+
+import 'package:rattle/constants/keys.dart';
+import 'package:rattle/features/summary/panel.dart';
+import 'package:rattle/main.dart' as app;
+import 'package:rattle/features/dataset/button.dart';
+import 'package:rattle/features/dataset/popup.dart';
+import 'package:rattle/tabs/explore.dart';
+
+/// A duration to allow the tester to view/interact with the testing. 5s is
+/// good, 10s is useful for development and 0s for ongoing. This is not
+/// necessary but it is handy when running interactively for the user running
+/// the test to see the widgets for added assurance. The PAUSE environment
+/// variable can be used to override the default PAUSE here:
+///
+/// flutter test --device-id linux --dart-define=PAUSE=0 integration_test/app_test.dart
+///
+/// 20230712 gjw
+
+const String envPAUSE = String.fromEnvironment('PAUSE', defaultValue: '0');
+final Duration pause = Duration(seconds: int.parse(envPAUSE));
+const Duration delay = Duration(seconds: 1);
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets('Home page loads okay.', (WidgetTester tester) async {
+    debugPrint('EXPLORE: Start up the app');
+
+    app.main();
+
+    // Trigger a frame. Finish animation and scheduled microtasks.
+
+    await tester.pumpAndSettle();
+
+    // Leave time to see the first page.
+
+    await tester.pump(pause);
+
+    final datasetButtonFinder = find.byType(DatasetButton);
+    expect(datasetButtonFinder, findsOneWidget);
+    await tester.pump(pause);
+
+    debugPrint('EXPLORE: Tap the Dataset button.');
+
+    final datasetButton = find.byType(DatasetButton);
+    expect(datasetButton, findsOneWidget);
+    await tester.pump(pause);
+    await tester.tap(datasetButton);
+    await tester.pumpAndSettle();
+
+    await tester.pump(delay);
+
+    debugPrint('EXPLORE: Tap the Demo button.');
+
+    final datasetPopup = find.byType(DatasetPopup);
+    expect(datasetPopup, findsOneWidget);
+    final demoButton = find.text('Demo');
+    expect(demoButton, findsOneWidget);
+    await tester.tap(demoButton);
+    await tester.pumpAndSettle();
+    await tester.pump(pause);
+
+    // Find the Explore tab by icon and tap on it.
+
+    final exploreIconFinder = find.byIcon(Icons.insights);
+    expect(exploreIconFinder, findsOneWidget);
+
+    // Tap the Explore tab.
+
+    await tester.tap(exploreIconFinder);
+    await tester.pumpAndSettle();
+
+    // Verify if the ExploreTabs widget is shown.
+
+    expect(find.byType(ExploreTabs), findsOneWidget);
+
+    // Navigate to the Explore tab.
+
+    final exploreTabFinder = find.text('Explore');
+    await tester.tap(exploreTabFinder);
+    await tester.pumpAndSettle();
+
+    // Find the Summary tab by its title.
+
+    final summaryTabFinder = find.text('Summary');
+    expect(summaryTabFinder, findsOneWidget);
+
+    // Tap the Summary tab.
+
+    await tester.tap(summaryTabFinder);
+    await tester.pumpAndSettle();
+
+    // Verify that the SummaryPanel is shown.
+
+    expect(find.byType(SummaryPanel), findsOneWidget);
+
+    // Find the button by its text.
+
+    final generateSummaryButtonFinder = find.text('Generate Dataset Summary');
+    expect(generateSummaryButtonFinder, findsOneWidget);
+
+    // Tap the button.
+
+    await tester.tap(generateSummaryButtonFinder);
+    await tester.pumpAndSettle();
+
+    // Find the right arrow button in the PageIndicator.
+
+    final rightArrowFinder = find.byIcon(Icons.arrow_right_rounded);
+    expect(rightArrowFinder, findsOneWidget);
+
+    // Tap the right arrow button to go to "Summary of the Dataset" page.
+
+    await tester.tap(rightArrowFinder);
+    await tester.pumpAndSettle();
+
+    // Find the text containing "2007-11-01".
+
+    final dateFinder = find.textContaining('2007-11-01');
+    expect(dateFinder, findsOneWidget);
+
+    // Find the text containing "39.800".
+
+    final valueFinder = find.textContaining('39.800');
+    expect(valueFinder, findsOneWidget);
+
+    // Tap the right arrow button to go to "Skim of the Dataset" page.
+
+    await tester.tap(rightArrowFinder);
+    await tester.pumpAndSettle();
+
+    // Find the text containing "366" as the number of rows.
+
+    final rowsFinder = find.textContaining('366');
+    expect(rowsFinder, findsOneWidget);
+
+    // Find the text containing "23" as the number of columns.
+
+    final columnsFinder = find.textContaining('23');
+    expect(columnsFinder, findsOneWidget);
+
+    // Tap the right arrow button to go to "Kurtosis and Skewness" page.
+
+    await tester.tap(rightArrowFinder);
+    await tester.pumpAndSettle();
+
+    // Find the text containing "-1.12569017" as the min_temp.
+
+    final tempMinFinder = find.textContaining('-1.12569017');
+    expect(tempMinFinder, findsOneWidget);
+
+    // Find the text containing "0.347510625" as the max_temp.
+
+    final tempMaxFinder = find.textContaining('0.347510625');
+    expect(tempMaxFinder, findsOneWidget);
+
+    debugPrint('EXPLORE: Finished.');
+  });
+}
