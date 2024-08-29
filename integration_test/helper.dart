39fea180833404382d81a43b328e101be0fad2f1
kev <u7274552@anu.edu.au>
2024-08-29 21:07:30 +1000

    add docs

--- integration_test/helper.dart
+++ integration_test/helper.dart
@@ -1,8 +1,33 @@
-// helper.dart
+/// Helper functions for integration tests.
+//
+// Time-stamp: <Tuesday 2024-08-27 20:54:02 +0800 Graham Williams>
+//
+/// Copyright (C) 2023-2024, Togaware Pty Ltd
+///
+/// Licensed under the GNU General Public License, Version 3 (the "License");
+///
+/// License: https://www.gnu.org/licenses/gpl-3.0.en.html
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <https://www.gnu.org/licenses/>.
+///
+/// Authors: Kevin Wang
+library;
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
-import 'package:rattle/features/correlation/panel.dart';
+
+// import 'package:rattle/features/correlation/panel.dart';
 import 'package:rattle/tabs/explore.dart'; // Adjust imports as necessary
 
 Future<void> verifyTextContent(WidgetTester tester, String? value) async {
