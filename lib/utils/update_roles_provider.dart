3dc9cdf6ebe13e40b1c7966edff7d4ca7372d58f
yixiangyin <y1027020239@qq.com>
2024-09-03 22:28:32 +1000

    order imports

--- lib/utils/update_roles_provider.dart
+++ lib/utils/update_roles_provider.dart
@@ -28,11 +28,10 @@ library;
 // Group imports by dart, flutter, packages, local. Then alphabetically.
 
 import 'package:flutter/material.dart';
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'package:rattle/providers/selected.dart';
 import 'package:rattle/providers/selected2.dart';
-
 import 'package:rattle/providers/vars/roles.dart';
 import 'package:rattle/providers/stdout.dart';
 import 'package:rattle/providers/vars/types.dart';
