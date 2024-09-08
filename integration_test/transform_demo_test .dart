da319a0b031cd33c49bdc45b8356be40a057ff33
kev <u7274552@anu.edu.au>
2024-09-09 01:57:34 +1000

    demo added

--- integration_test/transform_demo_test .dart
+++ integration_test/transform_demo_test .dart
@@ -1,4 +1,4 @@
-/// Test the EXPLORE tab CORRELATION feature on the DEMO dataset.
+/// Test the Transform tab Impute/Rescale/Recode feature on the DEMO dataset.
 //
 // Time-stamp: <Tuesday 2024-09-03 09:07:59 +1000 Graham Williams>
 //
@@ -29,12 +29,15 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 import 'package:rattle/features/impute/panel.dart';
+import 'package:rattle/features/recode/panel.dart';
+import 'package:rattle/features/rescale/panel.dart';
 import 'package:rattle/main.dart' as app;
 
 import 'utils/delays.dart';
 import 'utils/navigate_to_tab.dart';
 import 'utils/navigate_to_transform.dart';
 import 'utils/open_demo_dataset.dart';
+import 'utils/open_large_dataset.dart';
 import 'utils/press_button.dart';
 import 'utils/verify_page_content.dart';
 import 'utils/verify_text.dart';
@@ -42,7 +45,7 @@ import 'utils/verify_text.dart';
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  group('Explore Demo Correlation:', () {
+  group('Transform DEMO:', () {
     testWidgets('build, page.', (WidgetTester tester) async {
       app.main();
       await tester.pumpAndSettle();
@@ -50,34 +53,84 @@ void main() {
 
       await openDemoDataset(tester);
       await navigateToTransformTab(tester);
-      await navigateToTab(tester, 'Inpute', ImputePanel);
 
-      await pressButton(tester, 'Impute Missing Values');
+      // Impute page.
 
-      // Verify the content of the page 1.
+      await navigateToTab(tester, 'Impute', ImputePanel);
+
+      await pressFirstButton(tester, 'Impute Missing Values');
+
+      // Verify the content of the page.
 
       await verifyPageContent(
         tester,
-        'Correlation - Numeric Data',
-        'pressure_9am            1.00',
+        'Dataset Summary',
+        '2007-11-01',
       );
       await verifyTextContent(
         tester,
-        'wind_speed_3pm         -0.36        -0.34        -0.27           1.00',
+        '1st Qu.:2008-01-31',
       );
       await verifyTextContent(
         tester,
-        'rainfall               -0.33        -0.25         0.15           0.06',
+        'Min.   :13.00',
       );
       await verifyTextContent(
         tester,
-        'wind_gust_speed        -0.54        -0.53        -0.35           0.69',
+        '1st Qu.:1015.4',
       );
 
-      // Verify the content of the page 2.
+      // Rescale page.
 
-      await verifyPageContent(tester, 'Variable Correlation Plot');
-      await tester.pump(pause);
+      await navigateToTab(tester, 'Rescale', RescalePanel);
+
+      await pressFirstButton(tester, 'Rescale Variable Values');
+
+      // Verify the content of the page.
+
+      await verifyPageContent(
+        tester,
+        'Dataset Summary',
+        '2007-11-01',
+      );
+      await verifyTextContent(
+        tester,
+        'Min.   :13.00',
+      );
+      await verifyTextContent(
+        tester,
+        'Median :1020.1',
+      );
+      await verifyTextContent(
+        tester,
+        'Max.   :39.800',
+      );
+
+      // Recode page.
+
+      await navigateToTab(tester, 'Recode', RecodePanel);
+
+      await pressFirstButton(tester, 'Recode Variable Values');
+
+      // Verify the content of the page.
+
+      await verifyPageContent(
+        tester,
+        'Dataset Summary',
+        '2007-11-01',
+      );
+      await verifyTextContent(
+        tester,
+        '1st Qu.:31.00',
+      );
+      await verifyTextContent(
+        tester,
+        'Median :39.00',
+      );
+      await verifyTextContent(
+        tester,
+        'Mean   :39.84',
+      );
     });
   });
 }
