7644bb5042771e138d673f52d7be708bf99331b7
zheyxu <79696631+zheyxu@users.noreply.github.com>
2024-09-08 13:14:39 +1000

    add nnet model parameters

--- lib/features/neural/config.dart
+++ lib/features/neural/config.dart
@@ -25,11 +25,20 @@
 library;
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'package:rattle/constants/app.dart';
+import 'package:rattle/constants/spacing.dart';
+import 'package:rattle/providers/hidden_neurons.dart';
+import 'package:rattle/providers/max_nwts.dart';
+import 'package:rattle/providers/nnet_maxit.dart';
+import 'package:rattle/providers/nnet_skip.dart';
+import 'package:rattle/providers/nnet_trace.dart';
 import 'package:rattle/r/source.dart';
 import 'package:rattle/widgets/activity_button.dart';
+import 'package:rattle/widgets/number_field.dart';
 
 /// The NEURAL tab config currently consists of just an ACTIVITY button.
 ///
@@ -43,8 +52,36 @@ class NeuralConfig extends ConsumerStatefulWidget {
 }
 
 class NeuralConfigState extends ConsumerState<NeuralConfig> {
+  // Controllers for the input fields.
+  final TextEditingController _hiddenNeuronsController =
+      TextEditingController();
+  final TextEditingController _maxNWtsController = TextEditingController();
+  final TextEditingController _maxitController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Dispose the controllers to free up resources.
+    _hiddenNeuronsController.dispose();
+    _maxNWtsController.dispose();
+    _maxitController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    // Checkbox state.
+    bool nnetTrace = ref.read(nnetTraceProvider.notifier).state;
+
+    bool nnetSkip = ref.read(nnetSkipProvider.notifier).state;
+
+    // Keep the value of text field.
+
+    _hiddenNeuronsController.text =
+        ref.read(hiddenNeuronsProvider.notifier).state.toString();
+    _maxNWtsController.text =
+        ref.read(maxNWtsProvider.notifier).state.toString();
+    _maxitController.text = ref.read(maxitProvider.notifier).state.toString();
+
     return Column(
       children: [
         // Space above the beginning of the configs.
@@ -61,11 +98,142 @@ class NeuralConfigState extends ConsumerState<NeuralConfig> {
 
             ActivityButton(
               onPressed: () async {
-                // Run the R scripts.
-                await rSource(context, ref, 'model_template');
-                await rSource(context, ref, 'model_build_neural_net');
+                // Perform manual validation.
+                String? hiddenNeuronsError =
+                    validateInteger(_hiddenNeuronsController.text, min: 1);
+                String? maxNWtsError =
+                    validateInteger(_maxNWtsController.text, min: 1);
+                String? maxitError =
+                    validateInteger(_maxitController.text, min: 1);
+
+                // Collect all errors.
+                List<String> errors = [
+                  if (hiddenNeuronsError != null)
+                    'Hidden Neurons: $hiddenNeuronsError',
+                  if (maxNWtsError != null) 'Max NWts: $maxNWtsError',
+                  if (maxitError != null) 'Maxit: $maxitError',
+                ];
+
+                // Check if there are any errors.
+                if (errors.isNotEmpty) {
+                  // Show a warning dialog if validation fails.
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: const Text('Validation Error'),
+                      content: Text(
+                        'Please ensure all input fields are valid before building the nnet model:\n\n${errors.join('\n')}',
+                      ),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text('OK'),
+                        ),
+                      ],
+                    ),
+                  );
+
+                  return;
+                } else {
+                  ref.read(nnetTraceProvider.notifier).state = nnetTrace;
+                  ref.read(nnetSkipProvider.notifier).state = nnetSkip;
+
+                  ref.read(hiddenNeuronsProvider.notifier).state =
+                      int.parse(_hiddenNeuronsController.text);
+                  ref.read(maxNWtsProvider.notifier).state =
+                      int.parse(_maxNWtsController.text);
+                  ref.read(maxitProvider.notifier).state =
+                      int.parse(_maxitController.text);
+
+                  // Run the R scripts.
+                  await rSource(context, ref, 'model_template');
+                  await rSource(context, ref, 'model_build_neural_net');
+                }
               },
               child: const Text('Build Neural Network'),
+            ),
+
+            configWidgetSpace,
+
+            const Text(
+              'Trace',
+              style: normalTextStyle,
+            ),
+            Checkbox(
+              value: nnetTrace,
+              onChanged: (value) {
+                setState(() {
+                  nnetTrace = value!;
+                  ref.read(nnetTraceProvider.notifier).state = value;
+                });
+              },
+            ),
+
+            configWidgetSpace,
+
+            const Text(
+              'skip',
+              style: normalTextStyle,
+            ),
+            Checkbox(
+              value: nnetSkip,
+              onChanged: (value) {
+                setState(() {
+                  nnetSkip = value!;
+                  ref.read(nnetSkipProvider.notifier).state = value;
+                });
+              },
+            ),
+          ],
+        ),
+        configTopSpace,
+        Row(
+          children: [
+            NumberField(
+              label: 'Hidden Neurons:',
+              key: const Key('hidden_neurons'),
+              controller: _hiddenNeuronsController,
+
+              tooltip: ''' Hidden neurons receive input from all the neurons 
+ in the previous layer (input layer) and apply a weighted sum 
+ of inputs, followed by an activation function.
+              ''',
+              inputFormatter:
+                  FilteringTextInputFormatter.digitsOnly, // Integers only
+              validator: (value) => validateInteger(value, min: 1),
+              stateProvider: hiddenNeuronsProvider,
+            ),
+            const SizedBox(width: 16),
+            NumberField(
+              label: 'Max NWts:',
+              key: const Key('max_NWts'),
+              controller: _maxNWtsController,
+
+              tooltip: ''' The maxNWts parameter in the nnet function 
+ controls the maximum number of weights allowed 
+ in the neural network model.
+              ''',
+              inputFormatter:
+                  FilteringTextInputFormatter.digitsOnly, // Integers only
+              validator: (value) => validateInteger(value, min: 1),
+              stateProvider: maxNWtsProvider,
+            ),
+            const SizedBox(width: 16),
+            NumberField(
+              label: 'Maxit:',
+              key: const Key('maxit'),
+              controller: _maxitController,
+
+              tooltip: ''' The maxit parameter in the nnet model 
+ controls the maximum number of iterations (or epochs) 
+ allowed during the training of the neural network.
+              ''',
+              inputFormatter:
+                  FilteringTextInputFormatter.digitsOnly, // Integers only
+              validator: (value) => validateInteger(value, min: 1),
+              stateProvider: maxitProvider,
             ),
           ],
         ),
