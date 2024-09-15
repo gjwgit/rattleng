7ff4c1598789b89b37ec4d0e5ccf4ff86c5e3ba5
zheyxu <79696631+zheyxu@users.noreply.github.com>
2024-09-15 13:41:06 +1000

    remove unused imports

--- integration_test/dataset_large_test.dart
+++ integration_test/dataset_large_test.dart
@@ -33,7 +33,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:rattle/providers/stdout.dart';
 
-import 'package:rattle/main.dart' as app;
 import 'utils/init_app.dart';
 
 /// 20230712 gjw We use a PAUSE duration to allow the tester to view/interact
