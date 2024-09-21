334426847f0960e3d6fd281d6dec1015863f5cbc
Bill ZHANG <36790218+Lutra-Fs@users.noreply.github.com>
2024-09-21 18:24:24 +1000

    reduce complexity for data processing workflow

--- lib/features/dataset/display.dart
+++ lib/features/dataset/display.dart
@@ -21,7 +21,7 @@
 // You should have received a copy of the GNU General Public License along with
 // this program.  If not, see <https://www.gnu.org/licenses/>.
 ///
-/// Authors: Graham Williams, Yixiang Yin
+/// Authors: Graham Williams, Yixiang Yin， Bo Zhang
 
 library;
 
@@ -61,305 +61,253 @@ class DatasetDisplay extends ConsumerStatefulWidget {
 }
 
 class _DatasetDisplayState extends ConsumerState<DatasetDisplay> {
-  Widget space = const SizedBox(
-    width: 10,
-  );
 
-  int typeFlex = 4;
-  int contentFlex = 3;
+  // Constants for layout
+
+  final Widget space = const SizedBox(width: 10);
+
+  final int typeFlex = 4;
+  final int contentFlex = 3;
 
   @override
   Widget build(BuildContext context) {
-    String path = ref.watch(pathProvider);
-    String stdout = ref.watch(stdoutProvider);
+
+    final String path = ref.watch(pathProvider);
+    final String stdout = ref.watch(stdoutProvider);
 
     List<Widget> pages = [showMarkdownFile(welcomeMsgFile, context)];
 
-    ////////////////////////////////////////////////////////////////////////
-
-    String content = '';
-    String title = '';
-
-    // For a TXT file we add a new page as a summary of that file - actually it
-    // shows the full contents.
+    // Handle different file types
 
     if (path.endsWith('.txt')) {
-      content = rExtract(stdout, '> cat(ds,');
-      title = '''
-
-      # Text Content
-
-      Generated using
-      [base::cat(ds)](https://www.rdocumentation.org/packages/base/topics/cat).
-
-      ''';
-
-      if (content.isNotEmpty) {
-        pages.add(
-          TextPage(
-            title: title,
-            content: '\n$content',
-          ),
-        );
-      }
-    }
-
-    ////////////////////////////////////////////////////////////////////////
-
-    if (path == weatherDemoFile || path.endsWith('.csv')) {
-      Map<String, Role> currentRoles = ref.read(rolesProvider);
-
-      // Extract variable information from the R console.
-
-      List<VariableInfo> vars = extractVariables(stdout);
-      List<String> highVars = extractLargeFactors(stdout);
-
-      // Initialise ROLES. Default to INPUT and identify TARGET, RISK,
-      // IDENTS. Also record variable types.
-
-      if (currentRoles.isEmpty && vars.isNotEmpty) {
-        // Default is INPUT unless the variable name begins with `risk_`.
-
-        for (var column in vars) {
-          ref.read(rolesProvider.notifier).state[column.name] = Role.input;
-          ref.read(typesProvider.notifier).state[column.name] =
-              isNumeric(column.type) ? Type.numeric : Type.categoric;
-
-          if (column.name.toLowerCase().startsWith('risk_')) {
-            ref.read(rolesProvider.notifier).state[column.name] = Role.risk;
-          }
-
-          if (column.name.toLowerCase().startsWith('ignore_')) {
-            ref.read(rolesProvider.notifier).state[column.name] = Role.ignore;
-          }
-
-          if (column.name.toLowerCase().startsWith('target_')) {
-            ref.read(rolesProvider.notifier).state[column.name] = Role.target;
-          }
-        }
-
-        // Treat the last variable as a TARGET by default. We will eventually
-        // implement Rattle heuristics to identify the TARGET if the final
-        // variable has more than 5 levels. If so we'll check if the first
-        // variable looks like a TARGET (another common practise) and if not
-        // then no TARGET will be identified by default.
-
-        if (getTarget(ref) == 'NULL') {
-          ref.read(rolesProvider.notifier).state[vars.last.name] = Role.target;
-        } else {
-          ref.read(rolesProvider.notifier).state[getTarget(ref)] = Role.target;
-        }
-
-        // Any variables that have a unique value for every row in the dataset
-        // is considered to be an IDENTifier.
-
-        for (var id in getUniqueColumns(ref)) {
-          ref.read(rolesProvider.notifier).state[id] = Role.ident;
-        }
-        for (var highVar in highVars) {
-          if (ref.read(rolesProvider.notifier).state[highVar] != Role.target) {
-            ref.read(rolesProvider.notifier).state[highVar] = Role.ignore;
-          }
-        }
-      }
-
-      // When a new row is added after transformation, initialise its role and
-      // update the role of the old variable
-
-      updateVariablesProvider(ref);
-
-      var headline = Padding(
-        padding: const EdgeInsets.all(6.0),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Expanded(
-              child: Text(
-                'Variable',
-                style: TextStyle(fontWeight: FontWeight.bold),
-                textAlign: TextAlign.left,
-              ),
-            ),
-            space,
-            const Expanded(
-              child: Text(
-                'Type',
-                style: TextStyle(fontWeight: FontWeight.bold),
-                textAlign: TextAlign.left,
-              ),
-            ),
-            Expanded(
-              flex: typeFlex,
-              child: const Text(
-                'Role',
-                style: TextStyle(fontWeight: FontWeight.bold),
-                textAlign: TextAlign.left,
-              ),
-            ),
-            space,
-            Expanded(
-              flex: contentFlex,
-              child: const Text(
-                'Content',
-                style: TextStyle(fontWeight: FontWeight.bold),
-                textAlign: TextAlign.left,
-              ),
-            ),
-          ],
-        ),
-      );
-
-      Widget dataline(columnName, dataType, content) {
-        // Generate the row for a data line.
-
-        // Truncate the content to fit one line. The text could wrap over two
-        // lines and so show more of the data, but our point here is more to
-        // have a reminder of the data to assist in deciding on the ROLE of each
-        // variable, not any real insight into the data which we leave to the
-        // SUMMARY feature.
-
-        int maxLength = 40;
-
-        // Extract substring of the first maxLength characters
-
-        String subStr = content.length > maxLength
-            ? content.substring(0, maxLength)
-            : content;
-
-        // Find the last comma in the substring
-
-        int lastCommaIndex = subStr.lastIndexOf(',') + 1;
-
-        content =
-            lastCommaIndex > 0 ? content.substring(0, lastCommaIndex) : subStr;
-        content += ' ...';
-
-        return Padding(
-          padding: const EdgeInsets.all(6.0),
-          child: LayoutBuilder(
-            builder: (BuildContext context, BoxConstraints constraints) {
-              return Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                    child: FittedBox(
-                      fit: BoxFit.scaleDown,
-                      // Aligns the content to the left
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        columnName,
-                        style: defaultTextStyle,
-                        // Ensure the text stays on one line
-                        maxLines: 1,
-                        // Adds ellipsis if text overflows
-                        overflow: TextOverflow.ellipsis,
-                        // Aligns the text within the Text widget to the left
-                        textAlign: TextAlign.left,
-                      ),
-                    ),
-                  ),
-
-                  space,
-
-                  Expanded(
-                    child: Text(dataType),
-                  ),
-                  Expanded(
-                    flex: typeFlex,
-                    child: Wrap(
-                      spacing: 5.0,
-                      runSpacing: choiceChipRowSpace,
-                      children: choices.map((choice) {
-                        return ChoiceChip(
-                          label: Text(choice.displayString),
-                          disabledColor: Colors.grey,
-                          selectedColor: Colors.lightBlue[200],
-                          backgroundColor: Colors.lightBlue[50],
-                          showCheckmark: false,
-                          shadowColor: Colors.grey,
-                          pressElevation: 8.0,
-                          elevation: 2.0,
-                          selected: remap(currentRoles[columnName]!, choice),
-                          onSelected: (bool selected) {
-                            setState(() {
-                              // The parameter selected can be false when a chip
-                              // is tapped when it is already selected.  In our
-                              // case we need do nothing else. That could be
-                              // useful as a toggle button!
-
-                              if (selected) {
-                                // Only one variable can be TARGET, RISK and
-                                // WEIGHT so any previous variable with that
-                                // role shold become INPUT.
-
-                                if (choice == Role.target ||
-                                    choice == Role.risk ||
-                                    choice == Role.weight) {
-                                  currentRoles.forEach((key, value) {
-                                    if (value == choice) {
-                                      ref
-                                          .read(rolesProvider.notifier)
-                                          .state[key] = Role.input;
-                                    }
-                                  });
-                                }
-                                ref
-                                    .read(rolesProvider.notifier)
-                                    .state[columnName] = choice;
-                                debugText('  $choice', columnName);
-                              }
-                            });
-                          },
-                        );
-                      }).toList(),
-                    ),
-                  ),
-                  Expanded(
-                    flex: contentFlex,
-                    child: Text(
-                      content,
-                      style: const TextStyle(fontSize: 14),
-                    ),
-                  ),
-                  // Hard code the Spacer when the screen width is large.
-                ],
-              );
-            },
-          ),
-        );
-      }
-
-      pages.add(
-        ListView.builder(
-          key: const Key('roles listView'),
-
-          // Add 1 for the extra header row.
-
-          itemCount: vars.length + 1,
-
-          itemBuilder: (context, index) {
-            // Both the header row and the regular row shares the same flex
-            // index.
-
-            if (index == 0) {
-              // Render the extra header row.
-
-              return headline;
-            } else {
-              // Regular data rows.  Adjust the index for regular data.
-
-              final variableIndex = index - 1;
-              String columnName = vars[variableIndex].name;
-              String dataType = vars[variableIndex].type;
-              String content = vars[variableIndex].details;
-
-              return dataline(columnName, dataType, content);
-            }
-          },
-        ),
-      );
+      _addTextFilePage(stdout, pages);
+    } else if (path == weatherDemoFile || path.endsWith('.csv')) {
+      _addDatasetPage(stdout, pages);
     }
 
     return Pages(children: pages);
+  }
+
+  // Add a page for text file content
+  
+  void _addTextFilePage(String stdout, List<Widget> pages) {
+    String content = rExtract(stdout, '> cat(ds,');
+    String title =
+        '# Text Content\n\nGenerated using [base::cat(ds)](https://www.rdocumentation.org/packages/base/topics/cat).';
+
+    if (content.isNotEmpty) {
+      pages.add(TextPage(title: title, content: '\n$content'));
+    }
+  }
+
+  // Add a page for dataset summary
+
+  void _addDatasetPage(String stdout, List<Widget> pages) {
+    Map<String, Role> currentRoles = ref.read(rolesProvider);
+    List<VariableInfo> vars = extractVariables(stdout);
+    List<String> highVars = extractLargeFactors(stdout);
+
+    _initializeRoles(vars, highVars, currentRoles);
+    updateVariablesProvider(ref);
+
+    pages.add(
+      ListView.builder(
+        key: const Key('roles listView'),
+        itemCount: vars.length + 1,
+        itemBuilder: (context, index) {
+          if (index == 0) {
+            return _buildHeadline();
+          } else {
+            return _buildDataLine(vars[index - 1], currentRoles);
+          }
+        },
+      ),
+    );
+  }
+
+  // Initialize roles for variables
+
+  void _initializeRoles(List<VariableInfo> vars, List<String> highVars,
+      Map<String, Role> currentRoles,) {
+    if (currentRoles.isEmpty && vars.isNotEmpty) {
+      for (var column in vars) {
+        _setInitialRole(column, ref);
+      }
+      _setTargetRole(vars, ref);
+      _setIdentRole(ref);
+      _setIgnoreRoleForHighVars(highVars, ref);
+    }
+  }
+
+  // Set initial role for a variable
+
+  void _setInitialRole(VariableInfo column, WidgetRef ref) {
+    String name = column.name.toLowerCase();
+    Role role = Role.input;
+    if (name.startsWith('risk_')) role = Role.risk;
+    if (name.startsWith('ignore_')) role = Role.ignore;
+    if (name.startsWith('target_')) role = Role.target;
+
+    ref.read(rolesProvider.notifier).state[column.name] = role;
+    ref.read(typesProvider.notifier).state[column.name] =
+        isNumeric(column.type) ? Type.numeric : Type.categoric;
+  }
+
+  // Set target role
+
+  void _setTargetRole(List<VariableInfo> vars, WidgetRef ref) {
+    String target = getTarget(ref);
+    if (target == 'NULL') {
+      ref.read(rolesProvider.notifier).state[vars.last.name] = Role.target;
+    } else {
+      ref.read(rolesProvider.notifier).state[target] = Role.target;
+    }
+  }
+
+  // Set identifier role
+
+  void _setIdentRole(WidgetRef ref) {
+    for (var id in getUniqueColumns(ref)) {
+      ref.read(rolesProvider.notifier).state[id] = Role.ident;
+    }
+  }
+
+  // Set ignore role for high cardinality variables
+
+  void _setIgnoreRoleForHighVars(List<String> highVars, WidgetRef ref) {
+    for (var highVar in highVars) {
+      if (ref.read(rolesProvider.notifier).state[highVar] != Role.target) {
+        ref.read(rolesProvider.notifier).state[highVar] = Role.ignore;
+      }
+    }
+  }
+
+  // Build headline for the dataset summary
+  
+  Widget _buildHeadline() {
+    return Padding(
+      padding: const EdgeInsets.all(6.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Expanded(
+              child: Text('Variable',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.left,),),
+          space,
+          const Expanded(
+              child: Text('Type',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.left,),),
+          Expanded(
+              flex: typeFlex,
+              child: const Text('Role',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.left,),),
+          space,
+          Expanded(
+              flex: contentFlex,
+              child: const Text('Content',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.left,),),
+        ],
+      ),
+    );
+  }
+
+  // Build data line for each variable
+  Widget _buildDataLine(VariableInfo variable, Map<String, Role> currentRoles) {
+    String content = _truncateContent(variable.details);
+
+    return Padding(
+      padding: const EdgeInsets.all(6.0),
+      child: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          return Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(child: _buildFittedText(variable.name)),
+              space,
+              Expanded(child: Text(variable.type)),
+              Expanded(
+                  flex: typeFlex,
+                  child: _buildRoleChips(variable.name, currentRoles),),
+              Expanded(
+                  flex: contentFlex,
+                  child: Text(content, style: const TextStyle(fontSize: 14)),),
+            ],
+          );
+        },
+      ),
+    );
+  }
+
+  // Build fitted text for variable name
+  Widget _buildFittedText(String text) {
+    return FittedBox(
+      fit: BoxFit.scaleDown,
+      alignment: Alignment.centerLeft,
+      child: Text(
+        text,
+        style: defaultTextStyle,
+        maxLines: 1,
+        overflow: TextOverflow.ellipsis,
+        textAlign: TextAlign.left,
+      ),
+    );
+  }
+
+  // Build role choice chips
+  Widget _buildRoleChips(String columnName, Map<String, Role> currentRoles) {
+    return Wrap(
+      spacing: 5.0,
+      runSpacing: choiceChipRowSpace,
+      children: choices.map((choice) {
+        return ChoiceChip(
+          label: Text(choice.displayString),
+          disabledColor: Colors.grey,
+          selectedColor: Colors.lightBlue[200],
+          backgroundColor: Colors.lightBlue[50],
+          showCheckmark: false,
+          shadowColor: Colors.grey,
+          pressElevation: 8.0,
+          elevation: 2.0,
+          selected: remap(currentRoles[columnName]!, choice),
+          onSelected: (bool selected) =>
+              _handleRoleSelection(selected, choice, columnName, currentRoles),
+        );
+      }).toList(),
+    );
+  }
+
+  // Handle role selection
+  void _handleRoleSelection(bool selected, Role choice, String columnName,
+      Map<String, Role> currentRoles,) {
+    setState(() {
+      if (selected) {
+        if (choice == Role.target ||
+            choice == Role.risk ||
+            choice == Role.weight) {
+          currentRoles.forEach((key, value) {
+            if (value == choice) {
+              ref.read(rolesProvider.notifier).state[key] = Role.input;
+            }
+          });
+        }
+        ref.read(rolesProvider.notifier).state[columnName] = choice;
+        debugText('  $choice', columnName);
+      }
+    });
+  }
+
+  // Truncate content for display
+  String _truncateContent(String content) {
+    int maxLength = 40;
+    String subStr =
+        content.length > maxLength ? content.substring(0, maxLength) : content;
+    int lastCommaIndex = subStr.lastIndexOf(',') + 1;
+    return '${lastCommaIndex > 0
+            ? content.substring(0, lastCommaIndex)
+            : subStr} ...';
   }
 }
