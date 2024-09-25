fdf44abd26cf72bfefdb6a743d879932b2d9a229
kev <u7274552@anu.edu.au>
2024-09-25 10:33:16 +1000

    Explore Tests done

--- lib/features/tests/display.dart
+++ lib/features/tests/display.dart
@@ -29,8 +29,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:rattle/constants/markdown.dart';
+import 'package:rattle/providers/page_controller.dart';
 import 'package:rattle/providers/stdout.dart';
 import 'package:rattle/r/extract.dart';
+import 'package:rattle/widgets/page_viewer.dart';
 import 'package:rattle/widgets/pages.dart';
 import 'package:rattle/widgets/show_markdown_file.dart';
 import 'package:rattle/widgets/text_page.dart';
@@ -49,6 +51,9 @@ class _TestsDisplayState extends ConsumerState<TestsDisplay> {
 
   @override
   Widget build(BuildContext context) {
+    final pageController = ref
+        .watch(pageControllerProvider); // Get the PageController from Riverpod
+
     String stdout = ref.watch(stdoutProvider);
 
     List<Widget> pages = [showMarkdownFile(testsIntroFile, context)];
@@ -173,6 +178,9 @@ class _TestsDisplayState extends ConsumerState<TestsDisplay> {
 
     ////////////////////////////////////////////////////////////////////////
 
-    return Pages(children: pages);
+    return PageViewer(
+      pageController: pageController,
+      pages: pages,
+    );
   }
 }
