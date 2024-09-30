4b5927e52f02c8909faac54fb4fed89aafcb28be
Graham Williams <graham.williams@togaware.com>
2024-10-01 08:47:45 +1000

    Add note re navigation delay attempt.

--- lib/features/correlation/config.dart
+++ lib/features/correlation/config.dart
@@ -5,7 +5,7 @@
 /// License: GNU General Public License, Version 3 (the "License")
 /// https://www.gnu.org/licenses/gpl-3.0.en.html
 //
-// Time-stamp: <Tuesday 2024-10-01 08:40:41 +1000 Graham Williams>
+// Time-stamp: <Tuesday 2024-10-01 08:47:27 +1000 Graham Williams>
 //
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
@@ -64,6 +64,11 @@ class CorrelationConfigState extends ConsumerState<CorrelationConfig> {
               onPressed: () {
                 rSource(context, ref, ['explore_correlation']);
                 // TODO 20241001 kev WAIT FOR 3 SECONDS BEFORE MOVING TO THE NEXT PAGE.
+                //
+                // 20241001 gjw I don't think this works. The second page does
+                // not get navigated to after the first button push , even
+                // waiting 3s. But a second button push works fine.
+                //
                 Future.delayed(const Duration(seconds: 3));
                 ref.read(correlationPageControllerProvider).animateToPage(
                       // Index of the second page.
