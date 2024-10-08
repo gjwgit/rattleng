b977e0cc9530bd3072c2a255ddad41fccc467345
Graham Williams <graham.williams@togaware.com>
2024-10-09 09:33:11 +1100

    Review and comments.

--- lib/features/boost/display.dart
+++ lib/features/boost/display.dart
@@ -1,11 +1,11 @@
-/// Widget to display the BOOST introduction or output.
+/// Widget to display the BOOST introduction and outputs.
 ///
 /// Copyright (C) 2024, Togaware Pty Ltd.
 ///
 /// License: GNU General Public License, Version 3 (the "License")
 /// https://www.gnu.org/licenses/gpl-3.0.en.html
 //
-// Time-stamp: <Tuesday 2024-10-08 20:58:21 +1100 Graham Williams>
+// Time-stamp: <Wednesday 2024-10-09 09:31:18 +1100 Graham Williams>
 //
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
@@ -23,7 +23,7 @@
 // Note: The "Dataset" button is not present in this file. It might be located
 // in another file responsible for the app's navigation or main layout.
 ///
-/// Authors: Graham Williams, Zehyuan Xu
+/// Authors: Graham Williams, Zheyuan Xu
 
 library;
 
@@ -42,7 +42,7 @@ import 'package:rattle/widgets/pages.dart';
 import 'package:rattle/widgets/show_markdown_file.dart';
 import 'package:rattle/widgets/text_page.dart';
 
-/// The panel displays the instructions or the output.
+/// The panel displays the instructions and a navigator to the outputs.
 
 class BoostDisplay extends ConsumerStatefulWidget {
   const BoostDisplay({super.key});
@@ -55,8 +55,9 @@ class _BoostDisplayState extends ConsumerState<BoostDisplay> {
   @override
   Widget build(BuildContext context) {
     String stdout = ref.watch(stdoutProvider);
-
     String algorithm = ref.read(algorithmBoostProvider.notifier).state;
+
+    // Populate the initial overview
 
     List<Widget> pages = [showMarkdownFile(boostIntroFile, context)];
 
