c212e959d0cc17c6d5bb22ffdfa240b323a9efae
Graham Williams <graham.williams@togaware.com>
2024-10-15 09:19:50 +1100

    Rename workflow to build installers.

--- installers/update.sh
+++ installers/update.sh
@@ -8,8 +8,8 @@ if [ "$(gh run list --limit 1 --json databaseId,status --jq '.[0].status')" = "c
     # Identify the latest Bump Version but one. The latest is now the
     # integration test, and the one before it is the installer build.
 
-    bumpId=$(gh run list --limit 100 --json databaseId,displayTitle \
-		 | jq -r '.[] | select(.displayTitle | startswith("Bump version")) | .databaseId' | head -n 2 | tail -1)
+    bumpId=$(gh run list --limit 100 --json databaseId,displayTitle,workflowName \
+		 | jq -r '.[] | select(.workflowName | startswith("Build Installers")) | select(.displayTitle | startswith("Bump version")) | .databaseId' | head -n 1)
 
     # Determine the latest version. Assumes the latest action is a
     # Bump veriosn push.
