2d476f7002ac279c3989d8552e95d43ffb0d0ce2
zheyxu <79696631+zheyxu@users.noreply.github.com>
2024-11-12 13:45:14 +1100

    update display

--- lib/features/svm/display.dart
+++ lib/features/svm/display.dart
@@ -20,7 +20,7 @@
 // You should have received a copy of the GNU General Public License along with
 // this program.  If not, see <https://www.gnu.org/licenses/>.
 ///
-/// Authors: Graham Williams
+/// Authors: Graham Williams, Zheyuan Xu
 
 library;
 
