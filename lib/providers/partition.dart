04d497360576ecbea9855a71906b9d216bbf8c96
Graham Williams <graham.williams@togaware.com>
2024-11-25 21:19:35 +1100

    Set PARTITION toggle on by default.

--- lib/providers/partition.dart
+++ lib/providers/partition.dart
@@ -5,7 +5,7 @@
 /// License: GNU General Public License, Version 3 (the "License")
 /// https://www.gnu.org/licenses/gpl-3.0.en.html
 //
-// Time-stamp: <Monday 2024-07-22 16:42:13 +1000 Graham Williams>
+// Time-stamp: <Monday 2024-11-25 21:12:44 +1100 Graham Williams>
 //
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
@@ -28,4 +28,4 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 // For COMP3430 2024 S2 set the default off since that is what they are asked to
 // do. Perhaps default off is good in general?
 
-final partitionProvider = StateProvider<bool>((ref) => false);
+final partitionProvider = StateProvider<bool>((ref) => true);
