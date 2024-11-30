b1000052f5627221f20ae6856c7532b5e603bbb0
Graham Williams <graham.williams@togaware.com>
2024-11-30 21:44:18 +1100

    Lint fix

--- lib/features/tree/config.dart
+++ lib/features/tree/config.dart
@@ -1,6 +1,6 @@
 /// Configuration for tree models.
 //
-// Time-stamp: <Saturday 2024-11-30 21:31:04 +1100 Graham Williams>
+// Time-stamp: <Saturday 2024-11-30 21:43:47 +1100 Graham Williams>
 //
 /// Copyright (C) 2024, Togaware Pty Ltd.
 ///
@@ -220,8 +220,11 @@ class TreeModelConfigState extends ConsumerState<TreeModelConfig> {
                     if (selectedAlgorithm == AlgorithmType.conditional) {
                       rSource(context, ref, ['model_build_ctree']);
                     } else {
-                      rSource(context, ref,
-                          ['model_build_rpart', 'model_build_riskchart']);
+                      rSource(
+                        context,
+                        ref,
+                        ['model_build_rpart', 'model_build_riskchart'],
+                      );
                     }
                   }
                 },
