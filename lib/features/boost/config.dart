de3df60ac1cb1e77074c052e162ae7c295f25b2b
Graham Williams <graham.williams@togaware.com>
2024-12-14 20:51:28 +1100

    Migrate to Flutter 3.27 Row/Column `spacing:` - BOOST

--- lib/features/boost/config.dart
+++ lib/features/boost/config.dart
@@ -5,7 +5,7 @@
 /// License: GNU General Public License, Version 3 (the "License")
 /// https://www.gnu.org/licenses/gpl-3.0.en.html
 //
-// Time-stamp: <Friday 2024-11-01 16:21:12 +1100 Graham Williams>
+// Time-stamp: <Saturday 2024-12-14 20:50:54 +1100 Graham Williams>
 //
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
@@ -54,13 +54,13 @@ class BoostConfigState extends ConsumerState<BoostConfig> {
   Map<String, String> boostAlgorithm = {
     'Extreme': '''
 
-      A highly efficient gradient boosting algorithm designed for large-scale 
+      A highly efficient gradient boosting algorithm designed for large-scale
       and complex data.
 
       ''',
     'Adaptive': '''
 
-      A boosting algorithm that builds a strong classifier by iteratively 
+      A boosting algorithm that builds a strong classifier by iteratively
       combining weak learners, focusing on errors.
 
       ''',
@@ -71,12 +71,14 @@ class BoostConfigState extends ConsumerState<BoostConfig> {
     String algorithm = ref.read(algorithmBoostProvider.notifier).state;
 
     return Column(
+      spacing: configRowSpace,
       children: [
         // Space above the beginning of the configs.
 
         configTopGap,
 
         Row(
+          spacing: configWidgetSpace,
           children: [
             // Space to the left of the configs.
 
@@ -113,18 +115,7 @@ class BoostConfigState extends ConsumerState<BoostConfig> {
               child: const Text('Build Boosted Trees'),
             ),
 
-            configWidgetGap,
-
             Text('Target: ${getTarget(ref)}'),
-
-            configWidgetGap,
-
-            const Text(
-              'Algorithm:',
-              style: normalTextStyle,
-            ),
-
-            configWidgetGap,
 
             ChoiceChipTip<String>(
               options: boostAlgorithm.keys.toList(),
@@ -141,7 +132,6 @@ class BoostConfigState extends ConsumerState<BoostConfig> {
             ),
           ],
         ),
-        configRowGap,
         BoostSettings(algorithm: algorithm),
         configBotGap,
       ],
