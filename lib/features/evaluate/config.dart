4d36f8fd2556e988ddf945a021c4b203d02c706e
Graham Williams <graham.williams@togaware.com>
2024-12-20 20:50:42 +1100

    Demo use of tr and te and tu for evaluation.

--- lib/features/evaluate/config.dart
+++ lib/features/evaluate/config.dart
@@ -5,7 +5,7 @@
 /// License: GNU General Public License, Version 3 (the "License")
 /// https://www.gnu.org/licenses/gpl-3.0.en.html
 //
-// Time-stamp: <Friday 2024-12-20 20:49:24 +1100 Graham Williams>
+// Time-stamp: <Friday 2024-12-20 20:50:36 +1100 Graham Williams>
 //
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
@@ -246,7 +246,7 @@ class EvaluateConfigState extends ConsumerState<EvaluateConfig> {
                   await rSource(
                     context,
                     ref,
-                    [ec, ttu, em, ro],
+                    [ec, ttr, em, ro],
                   );
                 }
 
