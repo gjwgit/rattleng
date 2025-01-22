1b10f3b5a9373765828022073bb7f36cbb862e58
KevWindows <kevinwjh5200@gmail.com>
2025-01-23 09:45:12 +1100

    xgboost done

--- integration_test/weather_model_boost_xgboost.dart
+++ integration_test/weather_model_boost_xgboost.dart
@@ -51,35 +51,28 @@ void main() {
       await tester.pump(interact);
 
       await loadDemoDataset(tester);
+      await tester.pump(delay);
 
       await navigateToTab(tester, 'Model');
       await navigateToFeature(tester, 'Boost', BoostPanel);
 
       await tapButton(tester, 'Build Boosted Trees');
-      await tester.pump(longHack);
 
-      await tapButton(tester, 'Build Boosted Trees');
+      await tester.pump(delay);
+
+      await gotoNextPage(tester);
 
       // Verify the content of the page.
 
       await verifyPage(
         'XGBoost - Summary',
-        'Feature         Gain        Cover    Frequency   Importance',
       );
 
       await gotoNextPage(tester);
-      await tester.pump(hack);
 
-      final imagePageTitleFinder = find.text('Variable Importance');
-      expect(imagePageTitleFinder, findsOneWidget);
+      await verifyPage('Variable Importance');
 
-      final imageFinder = find.byType(ImagePage);
-
-      // Assert that the image is present.
-
-      expect(imageFinder, findsOneWidget);
-
-      await tester.pump(interact);
+      await verifyImage(tester);
     });
   });
 }
