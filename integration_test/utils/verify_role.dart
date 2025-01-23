ff81e0e79007873f8ec3345e411f86923089709f
KevWindows <kevinwjh5200@gmail.com>
2025-01-24 08:54:31 +1100

    add comments

--- integration_test/utils/verify_role.dart
+++ integration_test/utils/verify_role.dart
@@ -4,18 +4,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 Future<void> verifyRole(String variable, String role) async {
-  // Find the Text widget containing the variable name
+  // Find the Text widget containing the variable name.
+
   final variableFinder = find.text(variable).first;
 
-  // Find the parent Row widget that contains both variable name and role chips
+  // Find the parent Row widget that contains both variable name and role chips.
+
   final parentFinder = find.ancestor(
     of: variableFinder,
     matching: find.byType(Row),
   );
 
-  // Verify that both variable and role exist
+  // Verify that both variable and role exist.
+
   expect(variableFinder, findsOneWidget);
-  // Verify that the role chip exists and is selected
+  // Verify that the role chip exists and is selected.
+
   final choiceChip = find
       .descendant(
         of: parentFinder.first,
