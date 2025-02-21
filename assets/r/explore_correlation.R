bf98df86870cde7df4af97285e7f16793cdbad40
Graham Williams <graham.williams@togaware.com>
2025-02-22 09:08:31 +1100

    Bump version 6.4.18 - trial new correlation

--- assets/r/explore_correlation.R
+++ assets/r/explore_correlation.R
@@ -5,7 +5,7 @@
 # License: GNU General Public License, Version 3 (the "License")
 # https://www.gnu.org/licenses/gpl-3.0.en.html
 #
-# Time-stamp: <Saturday 2025-02-22 07:52:17 +1100 Graham Williams>
+# Time-stamp: <Saturday 2025-02-22 09:04:44 +1100 Graham Williams>
 #
 # Licensed under the GNU General Public License, Version 3 (the "License");
 #
@@ -52,8 +52,10 @@ cor <- cor[ord, ord]
 ## 20250222 gjw On Windows the layout is misaligned. we might try some
 ## alternatives as in #903. It's not a Rattle issue per se. If you
 ## copy the table and paste into Notepad we get the same misalignment.
+##
+## print(round(cor,2))
 
-print(round(cor,2))
+print(format(round(cor, 2), nsmall=2), quote=FALSE)
 
 # Generate the chart.
 
