fd3db8c3a7ba38da5b75313c10cb870bd47f82e5
Graham Williams <graham.williams@togaware.com>
2025-02-25 18:36:31 +1100

    Enable neuralnet

--- lib/features/neural/config.dart
+++ lib/features/neural/config.dart
@@ -5,7 +5,7 @@
 /// License: GNU General Public License, Version 3 (the "License")
 /// https://www.gnu.org/licenses/gpl-3.0.en.html
 //
-// Time-stamp: <Monday 2025-02-03 16:05:35 +1100 Graham Williams>
+// Time-stamp: <Tuesday 2025-02-25 09:49:00 +1100 Graham Williams>
 //
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
@@ -55,15 +55,17 @@ class NeuralConfig extends ConsumerStatefulWidget {
 
 // 20250202 gjw For now neuralnet is not fully operational for our typical
 // classification tasks. Stay with just nnet for now.
+//
+// 20250225 gjw Restore nueralnet with examples from COMP3430 labs.
 
 class NeuralConfigState extends ConsumerState<NeuralConfig> {
   Map<String, String> neuralAlgorithm = {
-    // 'neuralnet': '''
+    'neuralnet': '''
 
-    // Supports multiple layers, ideal for complex patterns.
-    // Commonly used for deeper architectures.
+    Supports multiple layers, ideal for complex patterns.
+    Commonly used for deeper architectures.
 
-    // ''',
+    ''',
     'nnet': '''
 
     A basic neural network with a single hidden layer.
