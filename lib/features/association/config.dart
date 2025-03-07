919f46fcc47a1ebffe02708973f6a9ac37585007
Graham Williams <graham.williams@togaware.com>
2025-03-07 14:48:02 +1100

    ASSOCIATIONS: Rule limit and sort should always be available

--- lib/features/association/config.dart
+++ lib/features/association/config.dart
@@ -5,7 +5,7 @@
 /// License: GNU General Public License, Version 3 (the "License")
 /// https://www.gnu.org/licenses/gpl-3.0.en.html
 //
-// Time-stamp: <Tuesday 2025-02-04 10:24:32 +1100 Graham Williams>
+// Time-stamp: <Friday 2025-03-07 14:45:38 +1100 Graham Williams>
 //
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
@@ -216,7 +216,11 @@ class AssociationConfigState extends ConsumerState<AssociationConfig> {
                 min: 2,
               ),
               stateProvider: interestMeasuresAssociationProvider,
-              enabled: !basketsTicked,
+
+              // 20250307 gjw Why was this only available when BASKETS was not
+              // ticked?
+
+              enabled: true, // !basketsTicked,
             ),
             variableChooser(
               'Sort by:',
@@ -235,7 +239,11 @@ class AssociationConfigState extends ConsumerState<AssociationConfig> {
                   ref.read(sortByAssociationProvider.notifier).state = value;
                 }
               },
-              enabled: !basketsTicked,
+
+              // 20250307 gjw Why was this only available when BASKETS was not
+              // ticked?
+
+              enabled: true, //!basketsTicked,
             ),
           ],
         ),
