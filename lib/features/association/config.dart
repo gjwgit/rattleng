678864d2583a712a476d2f0a3cd82aa839039458
Graham Williams <graham.williams@togaware.com>
2025-03-11 09:14:11 +1100

    ASSOCIATION: Support 10 decimal places for support/confidence

--- lib/features/association/config.dart
+++ lib/features/association/config.dart
@@ -5,7 +5,7 @@
 /// License: GNU General Public License, Version 3 (the "License")
 /// https://www.gnu.org/licenses/gpl-3.0.en.html
 //
-// Time-stamp: <Tuesday 2025-03-11 07:34:53 +1100 Graham Williams>
+// Time-stamp: <Tuesday 2025-03-11 08:55:43 +1100 Graham Williams>
 //
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
@@ -160,12 +160,12 @@ class AssociationConfigState extends ConsumerState<AssociationConfig> {
 
                 ''',
               inputFormatter: FilteringTextInputFormatter.allow(
-                RegExp(r'^[0-9]*\.?[0-9]{0,4}$'),
+                RegExp(r'^[0-9]*\.?[0-9]{0,10}$'),
               ),
               validator: (value) => validateDecimal(value),
               stateProvider: supportAssociationProvider,
               interval: 0.01,
-              decimalPlaces: 2,
+              decimalPlaces: 10,
             ),
             NumberField(
               label: 'Confidence:',
@@ -178,12 +178,12 @@ class AssociationConfigState extends ConsumerState<AssociationConfig> {
 
                 ''',
               inputFormatter: FilteringTextInputFormatter.allow(
-                RegExp(r'^[0-9]*\.?[0-9]{0,4}$'),
+                RegExp(r'^[0-9]*\.?[0-9]{0,10}$'),
               ),
               validator: (value) => validateDecimal(value),
               stateProvider: confidenceAssociationProvider,
               interval: 0.01,
-              decimalPlaces: 2,
+              decimalPlaces: 10,
             ),
             NumberField(
               label: 'Min Length:',
