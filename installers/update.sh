23aff225ecd152dafd891a005a8afd47608dbcd9
Graham Williams <graham.williams@togaware.com>
2025-03-14 20:04:12 +1100

    Revert installer to Bump Version from Build Inno.

--- installers/update.sh
+++ installers/update.sh
@@ -14,17 +14,17 @@ DEST=${HOST}:${FLDR}
 # version' pushes to the repositroy and get the latest one as the one
 # we want to download the artefacts.
 
-# bumpId=$(gh run list --limit 100 --json databaseId,displayTitle,workflowName \
-# 	     | jq -r '.[] | select(.workflowName | startswith("Build Installers")) | select(.displayTitle | startswith("Bump version")) | .databaseId' \
-# 	     | head -n 1)
+bumpId=$(gh run list --limit 100 --json databaseId,displayTitle,workflowName \
+	     | jq -r '.[] | select(.workflowName | startswith("Build Installers")) | select(.displayTitle | startswith("Bump version")) | .databaseId' \
+	     | head -n 1)
 
-# A temporary patch. COmment the above and uncomment below to do the
+# A temporary patch. Comment the above and uncomment below to do the
 # 'Build inno' path for a quick build of the windows inno exe for
 # debugging (gjw 20250314).
 
-bumpId=$(gh run list --limit 100 --json databaseId,displayTitle,workflowName \
-	     | jq -r '.[] | select(.workflowName | startswith("Build Installers")) | select(.displayTitle | startswith("Build inno")) | .databaseId' \
-	     | head -n 1)
+# bumpId=$(gh run list --limit 100 --json databaseId,displayTitle,workflowName \
+# 	     | jq -r '.[] | select(.workflowName | startswith("Build Installers")) | select(.displayTitle | startswith("Build inno")) | .databaseId' \
+# 	     | head -n 1)
 
 if [[ -z "${bumpId}" ]]; then
     echo "No workflow found."
