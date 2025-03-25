fbb7e9a7aa1498791ad6daeeb17d32a95995ca79
Graham Williams <graham.williams@togaware.com>
2025-03-26 05:36:56 +1100

    RECODE: Add chr -> fct R code.

---
+++ assets/r/transform_recode_as_categoric_character.R
@@ -0,0 +1,30 @@
+# Transform a `chr` into `fct`
+#
+# Copyright (C) 2025, Togaware Pty Ltd.
+#
+# License: GNU General Public License, Version 3 (the "License")
+# https://www.gnu.org/licenses/gpl-3.0.en.html
+#
+# Time-stamp: <Wednesday 2025-03-26 05:35:38 +1100 Graham Williams>
+#
+# Licensed under the GNU General Public License, Version 3 (the "License");
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Author: Graham Williams
+
+ds %<>% mutate(TFC_<SELECTED_VAR> = as.factor(<SELECTED_VAR>))
+
+glimpse(ds)
+summary(ds)
