6e7799c654d49412374b6ffb807063ff13309b0b
zheyxu <79696631+zheyxu@users.noreply.github.com>
2025-03-29 14:32:45 +1100

    update choice_chip

--- lib/widgets/choice_chip_tip.dart
+++ lib/widgets/choice_chip_tip.dart
@@ -37,6 +37,8 @@ class ChoiceChipTip<T> extends StatelessWidget {
   final ValueChanged<T?> onSelected;
   final Map? tooltips;
   final bool enabled;
+  final bool Function(T)? isOptionDisabled;
+
   const ChoiceChipTip({
     super.key,
     required this.options,
@@ -45,6 +47,7 @@ class ChoiceChipTip<T> extends StatelessWidget {
     this.tooltips,
     this.enabled = true,
     String Function(T)? getLabel,
+    this.isOptionDisabled,
   }) : getLabel = getLabel ?? _defaultGetLabel;
 
   static String _defaultGetLabel(option) => option.toString();
@@ -56,6 +59,9 @@ class ChoiceChipTip<T> extends StatelessWidget {
       runSpacing: choiceChipRowSpace,
       children: options.map((option) {
         final label = getLabel(option);
+        // Chip is disabled if the entire widget is disabled or the specific option is disabled,
+
+        final isChipDisabled = !enabled || (isOptionDisabled?.call(option) ?? false);
 
         return MarkdownTooltip(
           message: tooltips == null ? '' : tooltips![option] ?? '',
@@ -64,16 +70,17 @@ class ChoiceChipTip<T> extends StatelessWidget {
             showCheckmark: false,
             selectedColor: Colors.lightBlue[200],
             backgroundColor: Colors.lightBlue[50],
+            disabledColor: Colors.grey[300],
             shadowColor: Colors.grey,
             pressElevation: 8.0,
             elevation: 2.0,
             selected: selectedOption == option,
-            onSelected: enabled
-                ? (bool selected) {
+            onSelected: isChipDisabled
+                ? null // Disable interaction
+                : (bool selected) {
                     onSelected(selected ? option : null);
-                  }
-                : null,
-            // Selected chip to also have a black border.
+                  },
+            // Uncomment if you want a black border on selected chips
             // side: const BorderSide(
             //   color: Colors.black,
             //   width: 0.5,
