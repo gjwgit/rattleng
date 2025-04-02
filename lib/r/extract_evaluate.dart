c0bb339d1073834adbf73152e7177809820c4dd9
zheyxu <79696631+zheyxu@users.noreply.github.com>
2025-04-03 10:50:34 +1100

    remove clean_log

--- lib/r/extract_evaluate.dart
+++ lib/r/extract_evaluate.dart
@@ -24,12 +24,10 @@
 
 library;
 
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:rattle/providers/evaluate.dart';
 import 'package:rattle/providers/tree.dart';
-import 'package:rattle/r/clean_log.dart';
 import 'package:rattle/r/extract.dart';
 import 'package:rattle/utils/timestamp.dart';
 
