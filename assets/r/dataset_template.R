c89f6e3e02c7b138c717151be325d63f2c25caf0
Graham Williams <graham.williams@togaware.com>
2025-04-03 12:24:54 +1100

    Bug fix for NULL target in determining the list of vars.

--- assets/r/dataset_template.R
+++ assets/r/dataset_template.R
@@ -5,7 +5,7 @@
 # License: GNU General Public License, Version 3 (the "License")
 # https://www.gnu.org/licenses/gpl-3.0.en.html
 #
-# Time-stamp: <Tuesday 2025-03-25 16:11:57 +1100 Graham Williams>
+# Time-stamp: <Thursday 2025-04-03 12:20:57 +1100 Graham Williams>
 #
 # Licensed under the GNU General Public License, Version 3 (the "License");
 #
@@ -95,7 +95,7 @@ vars   <- names(ds)
 
 # Make the target variable the last one.
 
-vars %<>% '['(vars != target) %>% c(., target)
+if (!is.null(target)) vars %<>% '['(vars != target) %>% c(., target)
 
 # Identify the input variables for modelling.
 
