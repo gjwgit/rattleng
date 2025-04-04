94ff7ab1023cc7752f3172717e5d0ed24a8becd4
Graham Williams <graham.williams@togaware.com>
2025-04-04 13:54:44 +1100

    Stlye cleanup

--- assets/r/evaluate_measure_error_matrix.R
+++ assets/r/evaluate_measure_error_matrix.R
@@ -1,11 +1,11 @@
-# Using `actual` and `predicted` generate error matrix.
+# Using `actual` and `predicted` values generate two error matrices.
 #
-# Copyright (C) 2024, Togaware Pty Ltd.
+# Copyright (C) 2024-2025, Togaware Pty Ltd.
 #
 # License: GNU General Public License, Version 3 (the "License")
 # https://www.gnu.org/licenses/gpl-3.0.en.html
 #
-# Time-stamp: <Sunday 2025-02-02 14:47:05 +1100 Graham Williams>
+# Time-stamp: <Friday 2025-04-04 13:52:06 +1100 Graham Williams>
 #
 # Licensed under the GNU General Public License, Version 3 (the "License");
 #
@@ -24,6 +24,11 @@
 #
 # Author: Zheyuan Xu, Graham Williams
 
+# After a model is built we want to evaluate its performance. The
+# error matrix is a traditional way to do so. Here we generate two
+# matrices, one is the raw count of hte predictions and the other is
+# the percentages.
+#
 # <TIMESTAMP>
 #
 # References:
@@ -31,50 +36,40 @@
 # @williams:2017:essentials Chapter 7.
 # https://survivor.togaware.com/datascience/ for further details.
 
-####################################
-
-# 20250108 zy Add a check to ensure that the actual and
-# predicted have the same levels. To avoid every pred_ra
-# function to do this check, we do it here.
-
-# Combine the levels from both vectors.
+# Add a check to ensure that the actual and predicted have the same
+# levels. To avoid every pred_ra function to do this check, we do it
+# here. (zy 20250108)
+#
+# We combine the levels from both vectors, sort them if you want a
+# consistent order, and then use the levels for both variables.
 
 all_levels <- union(levels(actual_va), levels(predicted))
-
-# (Optional) Sort them if you want a consistent order.
-
 all_levels <- sort(all_levels)
+actual_va  <- factor(actual_va, levels=all_levels)
+predicted  <- factor(predicted, levels=all_levels)
 
-# Force both factors to share these levels.
-
-actual_va <- factor(actual_va,    levels = all_levels)
-predicted <- factor(predicted, levels = all_levels)
+# Next we build the error matrices.
 
 em_count <- rattle::errorMatrix(actual_va, predicted, count=TRUE)
+em_prop  <- rattle::errorMatrix(actual_va, predicted)
 
-# Capture the count error matrix as a string.
+## BEGIN RATTLE ONLY
+#
+# We process the error matrix into a string primarily for the benefit
+# of Rattle, and then write out a particular string to more easily
+# scrape the results from R into Rattle.
 
 em_count_str <- capture.output(print(em_count))
-
-# Combine the prefix with the captured output and print in one statement.
-
 writeLines(paste0('> ', mtype, "_<DATASET_TYPE>_COUNT\n", paste(em_count_str, collapse="\n")))
 
-# Generate a confusion matrix with proportions (relative frequencies)
-# rather than counts.
-
-em_prop <- rattle::errorMatrix(actual_va, predicted)
-
-# Capture the proportional error matrix as a string.
-
 em_prop_str <- capture.output(print(em_prop))
-
-# Combine the prefix with the captured output and print in one statement.
-
 writeLines(paste0('> ', mtype, "_<DATASET_TYPE>_PROP\n", paste(em_prop_str, collapse="\n")))
 
-# Exclude the "Error" column in the confusion matrix if it exists
-# Assuming the confusion matrix is a data frame with the last column as "Error".
+## END RATTLE ONLY
+
+# Exclude the "Error" column in the confusion matrix if it exists.
+# Assuming the confusion matrix is a data frame with the last column
+# as "Error".
 
 main_matrix <- em_count[, -ncol(em_count)]  # Remove the "Error" column
 
@@ -90,6 +85,8 @@ class_errors <- 1 - diag(main_matrix) / rowSums(main_matrix)
 
 avg_error <- mean(class_errors, na.rm = TRUE)
 
+## BEGIN RATTLE ONLY
+
 # Format error rates into a summary string.
 
 error_summary <- paste(
@@ -104,3 +101,5 @@ error_summary <- paste(
 # Log the formatted error summary showing overall and average error rates.
 
 writeLines(paste0('> ', mtype,  "_<DATASET_TYPE>_ERROR_MATRIX_SUMMARY: \n", paste(error_summary, collapse="\n")))
+
+## END RATTLE ONLY
