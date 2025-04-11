df98ee0bb2dbb2bfa01e38a21fda7e04eda21ba9
Graham Williams <graham.williams@togaware.com>
2025-04-12 09:57:20 +1000

    Update flutter prep targets and cleanup generally.

--- lib/r/extract_package.dart
+++ lib/r/extract_package.dart
@@ -1,3 +1,29 @@
+/// <DESCRIPTION>
+///
+// Time-stamp: <Saturday 2025-04-12 09:56:07 +1000 Graham Williams>
+///
+/// Copyright (C) 2025, Togaware Pty Ltd
+///
+/// Licensed under the GNU General Public License, Version 3 (the "License");
+///
+/// License: https://www.gnu.org/licenses/gpl-3.0.en.html
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <https://www.gnu.org/licenses/>.
+///
+/// Authors: <AUTHORS>
+
+library;
 // Function to parse the input text
 
 Map<String, List<String>> parsePackage(String text) {
