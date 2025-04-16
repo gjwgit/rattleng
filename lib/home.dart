021785ccefb32b4e355be7845f608c833c3a4df8
KevWindows <kevinwjh5200@gmail.com>
2025-04-16 10:34:05 +1000

    use 0.0.3 versionwidget

--- lib/home.dart
+++ lib/home.dart
@@ -428,35 +428,26 @@ Kevin Wang, Zheyuan Xu, Yixiang Yin, Bo Zhang.
         // Deploy the buttons aligned to the top right for actions.
 
         actions: [
-          // 20250113 gjw The version number is reported in the About popup but
-          // for screenshots, during development it is useful to have the
-          // version visiable at all times so place it on the title bar. Users
-          // have also noted it and seems useful to have it exposed.
-
-          // MarkdownTooltip(
-          //   message: '''
-
-          //   **Version:** ${_isLatest ? '''*Rattle* is regularly updated to bring you the best
-          //   experience for Data Science, AI and Machine Learning. The latest
-          //   version is always available from the
-          //   [Rattle](https://togaware.com/projects/rattle/) website. **Tap** on
-          //   the **Version** text here in the title bar to visit the *CHANGELOG*
-          //   in your browser and so see a list of all changes to Rattle.
-          //   ''' : '*A newer version is available!* Visit [Rattle](https://rattle.togaware.com) for instructions on updating your installation.'}
-
-          //   ''',
-          //   child: VersionWidget(
-          //     version: _appVersion,
-          //     changelogUrl: _changelogUrl,
-          //     showDate: true,
-          //     defaultDate: '20250101',
-          //   ),
-          // ),
           VersionWidget(
             version: _appVersion,
             changelogUrl: _changelogUrl,
             showDate: true,
             defaultDate: '20250101',
+            isLatestTooltip: '''
+            
+            *Rattle* is regularly updated to bring you the best
+            experience for Data Science, AI and Machine Learning. The latest
+            version is always available from the
+            [Rattle](https://togaware.com/projects/rattle/) website. **Tap** on
+            the **Version** text here in the title bar to visit the *CHANGELOG*
+            in your browser and so see a list of all changes to Rattle.
+
+            ''',
+            notLatestTooltip: '''
+
+            *A newer version is available!* Visit [Rattle](https://rattle.togaware.com) for instructions on updating your installation.
+            
+            ''',
           ),
 
           const SizedBox(width: 50),
