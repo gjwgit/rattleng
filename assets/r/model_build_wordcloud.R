f3832ffddb0abcacf7f209851b6abe71ff30c6c8
zheyxu <79696631+zheyxu@users.noreply.github.com>
2025-05-08 11:56:47 +1000

    remove duplicated code

--- assets/r/model_build_wordcloud.R
+++ assets/r/model_build_wordcloud.R
@@ -160,7 +160,6 @@ svg("<TEMPDIR>/word_frequency_barplot.svg")
 
 d %>% 
   dplyr::arrange(desc(freq)) %>%
-  dplyr::slice_head(n = 20) %>%
   dplyr::filter(freq >= <MINFREQ>) %>%
   ggplot2::ggplot(ggplot2::aes(x = reorder(word, freq), y = freq)) +
   ggplot2::geom_bar(stat = "identity", fill = "steelblue") +
