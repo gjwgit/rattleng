031eb3065823af08f249f415dd59713f52671046
Graham Williams <graham.williams@togaware.com>
2025-05-08 14:41:08 +1000

    TEST: Update cforest test.

--- integration_test/weather_model_cforest.dart
+++ integration_test/weather_model_cforest.dart
@@ -1,6 +1,6 @@
 /// WEATHER -> CFOREST
 //
-// Time-stamp: <Tuesday 2025-03-25 13:09:14 +1100 Graham Williams>
+// Time-stamp: <Thursday 2025-05-08 14:40:16 +1000 Graham Williams>
 //
 /// Copyright (C) 2024, Togaware Pty Ltd
 ///
@@ -54,17 +54,42 @@ void main() {
       await tapChip(tester, 'Conditional');
       await tapButton(tester, 'Build Random Forest');
       await addDelay(tester, 2);
-      await navigateToPage(tester, 1, back: 1, title: 'Random Forest Model');
+      await navigateToPage(
+        tester,
+        1,
+        back: 1,
+        title: 'Random Forest Model',
+      );
       await verifySelectableText(tester, [
         'Number of trees:  500',
         'Number of observations:  254',
       ]);
-      // 2025-02-14 10:24 gjw I tried testing the TITLE but could not get it to
-      // work for this page - 'Variable Importance'.
       await navigateToPage(
         tester,
         2,
         back: 1,
+        title: 'Sample Rules',
+      );
+      await verifySelectableText(tester, [
+        '1) pressure_9am <= 1018.3; criterion = 0.993, statistic = 24.293',
+        '2) wind_speed_3pm <= 22; criterion = 0.99, statistic = 17.177',
+        '3) max_temp <= 22.7; criterion = 0.972, statistic = 11.053',
+        '4)*  weights = 0 ',
+        '3) max_temp > 22.7',
+        '5) wind_gust_speed <= 44; criterion = 0.936, statistic = 3.442',
+        '6)*  weights = 0 ',
+        '5) wind_gust_speed > 44',
+        '7)*  weights = 0 ',
+        '2) wind_speed_3pm > 22',
+        '8) temp_3pm <= 21.3; criterion = 0.926, statistic = 10.5',
+        '9)*  weights = 0 ',
+        '8) temp_3pm > 21.3',
+        '10)*  weights = 0 ',
+      ]);
+      await navigateToPage(
+        tester,
+        3,
+        back: 2,
         title: 'Variable Importance — Numeric',
       );
       // 20250212 gjw Oddly on one failure `0.025935...` was not found, yet
@@ -77,21 +102,21 @@ void main() {
       await addDelay(tester, 4);
       await verifySelectableText(tester, [
         '                       Variable    Importance',
-        'humidity_3pm       humidity_3pm  0.0247096774',
-        'pressure_3pm       pressure_3pm  0.0120000000',
-        'pressure_9am       pressure_9am  0.0060645161',
-        'min_temp               min_temp  0.0036989247',
-        'wind_dir_3pm       wind_dir_3pm  0.0031182796',
-        'rainfall               rainfall  0.0018924731',
-        'wind_gust_speed wind_gust_speed  0.0017634409',
-        'wind_speed_3pm   wind_speed_3pm  0.0015483871',
-        'wind_gust_dir     wind_gust_dir  0.0011827957',
-        'wind_speed_9am   wind_speed_9am  0.0010967742',
-        'cloud_3pm             cloud_3pm  0.0007311828',
-        'humidity_9am       humidity_9am  0.0005161290',
-        'max_temp               max_temp  0.0004516129',
-        'temp_3pm               temp_3pm  0.0004516129',
-        'cloud_9am             cloud_9am -0.0001290323',
+        'humidity_3pm       humidity_3pm  5.000000e-02',
+        'pressure_3pm       pressure_3pm  2.821505e-02',
+        'pressure_9am       pressure_9am  4.903226e-03',
+        'wind_speed_3pm   wind_speed_3pm  3.182796e-03',
+        'wind_gust_speed wind_gust_speed  1.677419e-03',
+        'wind_dir_3pm       wind_dir_3pm  1.548387e-03',
+        'min_temp               min_temp  1.204301e-03',
+        'temp_3pm               temp_3pm  9.247312e-04',
+        'max_temp               max_temp  6.666667e-04',
+        'cloud_3pm             cloud_3pm  5.591398e-04',
+        'wind_gust_dir     wind_gust_dir  4.301075e-04',
+        'wind_speed_9am   wind_speed_9am  3.010753e-04',
+        'rainfall               rainfall  1.935484e-04',
+        'temp_9am               temp_9am  1.290323e-04',
+        'humidity_9am       humidity_9am  6.451613e-05',
       ]);
     });
   });
