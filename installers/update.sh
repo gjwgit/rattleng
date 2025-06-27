f83ccbd850515eff25c130401fbc9edf65abe59d
Graham Williams <graham.williams@togaware.com>
2025-06-27 15:31:35 +1000

    Update installer standards.

--- installers/update.sh
+++ installers/update.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 # 20241024 gjw After a github action has built the bundles and stored
-# them as artefacts on github, we can upload them to togaware.com for
+# them as artefacts on github, we can upload them to the ${HOST} for
 # distribution.
 
-APP=rattle
+APP=$(basename "$(dirname "$(pwd)")")
+APP=${APP::-2}
+REP=$(git remote get-url origin | sed -E 's#.*[/:]([^/]+)/[^/]+(\.git)?$#\1#')
 
 HOST=togaware.com
 FLDR=apps/access/
 DEST=${HOST}:${FLDR}
 
 # From the recent 'Build Installers' workflows, identify the 'Bump
-# version' pushes to the repositroy and get the latest one as the one
+# version' pushes to the repository and get the latest one as the one
 # we want to download the artefacts.
 
 bumpId=$(gh run list --limit 100 --json databaseId,displayTitle,workflowName \
@@ -26,15 +28,31 @@ bumpId=$(gh run list --limit 100 --json databaseId,displayTitle,workflowName \
 # 	     | jq -r '.[] | select(.workflowName | startswith("Build Installers")) | select(.displayTitle | startswith("Build inno")) | .databaseId' \
 # 	     | head -n 1)
 
+echo "github action id: $bumpId"
+
 if [[ -z "${bumpId}" ]]; then
     echo "No workflow found."
     exit 1
 fi
-echo ${bumpId}
+
 status=$(gh run view ${bumpId} --json status --jq '.status')
 conclusion=$(gh run view ${bumpId} --json conclusion --jq '.conclusion')
 
 # Only proceed if the latest action hase been completed successfully
+
+# 20250611 gjw Currently failing:
+#
+# gh run download ${bumpId} --name ${APP}-linux-zip
+# error downloading ${APP}-linux-zip: would result in path traversal
+#
+# I was then manually downloading through browser, unzip and move
+# here, then run this script.
+#
+# But this should work as an alternative:
+#
+# gh api -H "Accept: application/vnd.github+json"   repos/${REP}/${APP}/actions/artifacts/3300608315/zip >| artifact.zip
+#
+# Need to get the correct artifact ID for each artefact.
 
 if [[ "${status}" == "completed" && "${conclusion}" == "success" ]]; then
 
@@ -46,35 +64,49 @@ if [[ "${status}" == "completed" && "${conclusion}" == "success" ]]; then
 
     version=$(grep version ../pubspec.yaml | head -1 | cut -d ':' -f 2 | sed 's/ //g')
 
-    # 20250210 gjw Moving to deb install as my local installation. See
-    # file:../Makefile
-    #
-    # 20250218 gjw Remove the deb build until it is configured
-    # properly.
-    #
-    # 20250221 gjw Got deb build working. Do that install in the
-    # Makefile now and don't do the local unzip install here.
+    echo '***** UPLOAD LINUX ZIP.'
+
+    ## gh run download ${bumpId} --name ${APP}-linux-zip
+
+    artifactId=$(gh api -H "Accept: application/vnd.github+json" /repos/${REP}/${APP}/actions/artifacts \
+		    --jq '.artifacts[] | select(.name | endswith("-linux-zip")) | .id' | head -n 1)
+    echo "artifact id: $artifactId"
+    gh api -H "Accept: application/vnd.github+json" repos/${REP}/${APP}/actions/artifacts/${artifactId}/zip > artifact.zip
+    unzip artifact.zip
+    rm -f artifact.zip
+
+    rsync -avzh ${APP}-dev-linux.zip ${DEST}
+    mv -f ${APP}-dev-linux.zip ARCHIVE/${APP}_${version}_linux.zip
+
+    echo ""
 
     echo '***** UPLOAD WINDOWS INNO'
 
-    gh run download ${bumpId} --name ${APP}-windows-inno
+    ## gh run download ${bumpId} --name ${APP}-windows-inno
+
+    artifactId=$(gh api -H "Accept: application/vnd.github+json" /repos/${REP}/${APP}/actions/artifacts \
+		    --jq '.artifacts[] | select(.name | endswith("-windows-inno")) | .id' | head -n 1)
+    echo "artifact id: $artifactId"
+    gh api -H "Accept: application/vnd.github+json" repos/${REP}/${APP}/actions/artifacts/${artifactId}/zip > artifact.zip
+    unzip artifact.zip
+    rm -f artifact.zip
+
     rsync -avzh ${APP}-dev-windows-inno.exe ${DEST}
     mv ${APP}-dev-windows-inno.exe ARCHIVE/${APP}_${version}_windows-inno.exe
 
     echo ""
 
-    echo '***** UPLOAD LINUX ZIP. LOCAL INSTALL'
-
-    gh run download ${bumpId} --name ${APP}-linux-zip
-    rsync -avzh ${APP}-dev-linux.zip ${DEST}
-    # unzip -oq ${APP}-dev-linux.zip -d ${HOME}/.local/share/${APP}/
-    mv -f ${APP}-dev-linux.zip ARCHIVE/${APP}_${version}_linux.zip
-
-    echo ""
-
     echo '***** UPLOAD WINDOWS ZIP'
 
-    gh run download ${bumpId} --name ${APP}-windows-zip
+    ## gh run download ${bumpId} --name ${APP}-windows-zip
+
+    artifactId=$(gh api -H "Accept: application/vnd.github+json" /repos/${REP}/${APP}/actions/artifacts \
+		    --jq '.artifacts[] | select(.name | endswith("-windows-zip")) | .id' | head -n 1)
+    echo "artifact id: $artifactId"
+    gh api -H "Accept: application/vnd.github+json" repos/${REP}/${APP}/actions/artifacts/${artifactId}/zip > artifact.zip
+    unzip artifact.zip
+    rm -f artifact.zip
+
     rsync -avzh ${APP}-dev-windows.zip ${DEST}
     mv -f ${APP}-dev-windows.zip ARCHIVE/${APP}_${version}_windows.zip
     ssh ${HOST} "cd ${FLDR}; chmod a+r ${APP}-dev-*.zip ${APP}-dev-*.exe"
@@ -84,7 +116,15 @@ if [[ "${status}" == "completed" && "${conclusion}" == "success" ]]; then
 
     echo '***** UPLOAD MACOS'
 
-    gh run download ${bumpId} --name ${APP}-macos-zip
+    ## gh run download ${bumpId} --name ${APP}-macos-zip
+
+    artifactId=$(gh api -H "Accept: application/vnd.github+json" /repos/${REP}/${APP}/actions/artifacts \
+		    --jq '.artifacts[] | select(.name | endswith("-macos-zip")) | .id' | head -n 1)
+    echo "artifact id: $artifactId"
+    gh api -H "Accept: application/vnd.github+json" repos/${REP}/${APP}/actions/artifacts/${artifactId}/zip > artifact.zip
+    unzip artifact.zip
+    rm -f artifact.zip
+
     rsync -avzh ${APP}-dev-macos.zip ${DEST}
     mv ${APP}-dev-macos.zip ARCHIVE/${APP}_${version}_macos.zip
     ssh ${HOST} "cd ${FLDR}; chmod a+r ${APP}-dev-*.zip ${APP}-dev-*.exe"
