b8d0b9e3d098af79c5b478b6956e08325e93db20
Graham Williams <graham.williams@togaware.com>
2025-08-16 06:42:26 +1000

    LINT: Remove <AUTHORS>

--- lib/r/extract_package.dart
+++ lib/r/extract_package.dart
@@ -21,7 +21,7 @@
 // You should have received a copy of the GNU General Public License along with
 // this program.  If not, see <https://www.gnu.org/licenses/>.
 ///
-/// Authors: <AUTHORS>
+/// Authors:
 
 library;
 // Function to parse the input text
