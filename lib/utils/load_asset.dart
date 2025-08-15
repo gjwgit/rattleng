bf8e09c85e262addb7995a05c6d11cddf3dd57a2
Graham Williams <graham.williams@togaware.com>
2025-08-15 21:10:08 +1000

    Lint Copyright fixes

--- lib/utils/load_asset.dart
+++ lib/utils/load_asset.dart
@@ -1,6 +1,28 @@
-/// Load data from the assets folder.
+/// Rattle - Data Science Next Generation
 ///
-/// Authors: Zheyuan Xu, Graham Williams
+// Time-stamp: <Friday 2025-08-15 21:05:18 +1000 Graham Williams>
+///
+/// Copyright (C) 2025, Togaware Pty Ltd
+///
+/// Licensed under the GNU General Public License, Version 3 (the "License");
+///
+/// License: https://www.gnu.org/licenses/gpl-3.0.en.html
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see https://www.gnu.org/licenses/.
+///
+/// Authors: AUTHORS
+
 library;
 
 import 'package:flutter/services.dart' show rootBundle;
