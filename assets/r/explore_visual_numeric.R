7206b97bc9249f8a10ce500eb42040f4e3347f24
Graham Williams <graham.williams@togaware.com>
2025-08-15 13:40:08 +1000

    Bug fix and update tests.

--- assets/r/explore_visual_numeric.R
+++ assets/r/explore_visual_numeric.R
@@ -5,7 +5,7 @@
 # License: GNU General Public License, Version 3 (the "License")
 # https://www.gnu.org/licenses/gpl-3.0.en.html
 #
-# Time-stamp: <Friday 2025-08-15 13:26:55 +1000 Graham Williams>
+# Time-stamp: <Friday 2025-08-15 13:38:34 +1000 Graham Williams>
 #
 # Licensed under the GNU General Public License, Version 3 (the "License");
 #
@@ -99,15 +99,13 @@ dev.off()
 
 svg("<TEMPDIR>/explore_visual_histogram.svg", width=10)
 tds %>%
-  dplyr::select(min_temp, rain_tomorrow) %>%
-  ggplot2::ggplot(ggplot2::aes(x=min_temp)) +
-  ggplot2::geom_histogram(ggplot2::aes(fill=rain_tomorrow), alpha=0.55, position="identity", bins=30) +
-  ggplot2::xlab(paste("min_temp\n\n", paste("Rattle 2025-08-15 11:58:32", username), sep="")) +
-  ggplot2::xlab(paste("<GROUP_BY_VAR>\n\n", paste("<TIMESTAMP>", username), sep="")) +
+  dplyr::select(<SELECTED_VAR>, <GROUP_BY_VAR>) %>%
+  ggplot2::ggplot(ggplot2::aes(x=<SELECTED_VAR>)) +
+  ggplot2::geom_histogram(ggplot2::aes(fill=<GROUP_BY_VAR>), alpha=0.55, position="identity", bins=30) +
+  ggplot2::xlab(paste("<SELECTED_VAR>\n\n", paste("<TIMESTAMP>", username), sep="")) +
   ggplot2::ggtitle("Histogram of <SELECTED_VAR> by <GROUP_BY_VAR>") +
   ggplot2::labs(fill="<GROUP_BY_VAR>", y="Count") +
   <SETTINGS_GRAPHIC_THEME>()
-  ggthemes::theme_solarized()
 dev.off()
 
 ########################################################################
