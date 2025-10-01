d0cd501829491c47920a15ea5d4cae8fb6ae6964
Graham Williams <graham.williams@togaware.com>
2025-10-01 16:35:41 +1000

    Template update.

--- support/flutter.mk
+++ support/flutter.mk
@@ -138,7 +138,7 @@ linux_config:
 	flutter config --enable-linux-desktop
 
 .PHONY: prep
-prep: analyze fix import_order_fix format dcm ignore license todo locmax markdown lychee depend bakfind test
+prep: analyze fix import_order_fix format dcm ignore license todo locmax markdown lychee depend bakfind
 	@echo "ADVISORY: make tests docs"
 	@echo $(SEPARATOR)
 
