a09a327af4b665a5805cc0d410139c0114a87448
Graham Williams <graham.williams@togaware.com>
2026-01-18 11:59:18 +1100

    Begin migrate id of unique columns from scarping to meta data

--- lib/utils/get_unique_columns.dart
+++ lib/utils/get_unique_columns.dart
@@ -1,8 +1,8 @@
 /// Obtain list of variables that have a unique value.
 //
-// Time-stamp: "Sunday 2024-09-08 12:18:44 +1000 Graham Williams"
+// Time-stamp: "Sunday 2026-01-18 11:58:51 +1100 Graham Williams"
 //
-/// Copyright (C) 2024, Togaware Pty Ltd
+/// Copyright (C) 2024-2026, Togaware Pty Ltd
 ///
 /// Licensed under the GNU General Public License, Version 3 (the "License");
 ///
@@ -27,10 +27,19 @@ library;
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+// import 'package:rattle/providers/meta_data.dart';
 import 'package:rattle/providers/stdout.dart';
 import 'package:rattle/r/extract.dart';
 
 List<String> getUniqueColumns(WidgetRef ref) {
+  // 20260118 gjw I considered replacing the console scanning implementation
+  // using `unique_colums()` (whose definition we will now remove from the R
+  // code setup) with accessing the data from the meta data provider.  But I do
+  // not currently have the number of rows captured in a provider. Once that is
+  // captured we can use the meta data to determine the columns with unique
+  // values. This may be quicker than scanning the console for the output of
+  // `unique_columns()`
+
   String stdout = ref.watch(stdoutProvider);
 
   String uniqueColumns = rExtract(stdout, 'unique_columns(ds)');
@@ -42,6 +51,29 @@ List<String> getUniqueColumns(WidgetRef ref) {
   RegExp regExp = RegExp(r'"(.*?)"');
   Iterable<Match> matches = regExp.allMatches(uniqueColumns);
   List<String> ids = matches.map((match) => match.group(1)!).toList();
+
+  // Map metaData = ref.read(metaDataProvider);
+
+  // List<String> ids = [];
+
+  // final nRows = 365;
+
+  // for (var entry in metaData.entries) {
+  //   // Ensure the value is a Map and check the 'unique' key.
+
+  //   if (entry.value is Map<String, dynamic>) {
+  //     var uniqueList = (entry.value as Map<String, dynamic>)['unique'];
+
+  //     // Check if 'unique' is a List and if the first element is the same as the
+  //     // number of rows in the dataset.
+
+  //     if (uniqueList is List &&
+  //         uniqueList.isNotEmpty &&
+  //         uniqueList[0] == nRows) {
+  //       ids.add(entry.key);
+  //     }
+  //   }
+  // }
 
   return ids;
 }
