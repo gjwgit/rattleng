461b6f00081442100b105f61e45e8183d453accd
Graham Williams <graham.williams@togaware.com>
2026-04-22 11:09:59 +1000

    Add x axis labels to hierarchical plot.

--- assets/r/model_build_hclust.R
+++ assets/r/model_build_hclust.R
@@ -5,7 +5,7 @@
 # License: GNU General Public License, Version 3 (the "License")
 # https://www.gnu.org/licenses/gpl-3.0.en.html
 #
-# Time-stamp: <Wednesday 2025-05-14 17:06:07 +1000 Graham Williams>
+# Time-stamp: <Wednesday 2026-04-22 11:08:25 +1000 Graham Williams>
 #
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
@@ -135,7 +135,6 @@ ggplot() +
   labs(title = "Hierarchical Clustering Dendrogram", y="", x="") +
   <SETTINGS_GRAPHIC_THEME>() +
   theme(plot.margin = margin(10, 50, 10, 10),
-        axis.text.x = element_blank(),
         axis.text.y = element_blank() )
 dev.off()
 
